595c8e696e9242ee4f4dd7a1edb446b6ade0295b
Manoj Mathai <manoj@yournextleap.com>
2012-12-20 11:12:34 +0530

    Added support for remote leonidas resource for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 require 'vger/helpers/yoren_user_helper'
+require 'vger/spartan/vanguard/recruiter'
+require 'vger/yoren/user'
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
 
-  has_remote_model :yoren => {
+  has_remote_model :leonidas => {
+    :attributes => [],
+    :key_column => {:self => :sid, :remote => :user_id},
+    :wrapper => Vger::Spartan::Vanguard::Recruiter
+  },:yoren => {
     :attributes => [:sex, :login, :fb_user_id, :fb_email, :sid, :location_id, :name, :phone, :confirmed_at],
     :key_column => {:self => :email, :remote => :email},
     :wrapper => Vger::Yoren::User
@@ -16,5 +22,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :sid
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :sid, :name, :phone
+  #attr_accessor :name, :phone
 end
