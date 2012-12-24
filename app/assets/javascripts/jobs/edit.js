e72ffcd1eb23aca6f7f65117258a0cdf049cb5c6
Bongs <saurabh@yournextleap.com>
2012-12-24 10:25:40 +0530

    Job posting flow fixes
    - save and continue later implemented
    - init master data code refactored

--- app/assets/javascripts/jobs/edit.js
+++ app/assets/javascripts/jobs/edit.js
@@ -422,6 +422,13 @@ $(function() {
   }
   setUneditableContactDetails();
 
+  $("[data-continue_later]").on("click", function(e) {
+    var form = $("[data-job_posting_container]").find("[data-job_posting_form]");
+    form.find("input[name='continue_later']").val("true");
+    form.submit();
+    e.preventDefault();
+  })
+
   // $("select[data-job-category]").on("change", function() {
   // 	// console.log($(this));
   // 	initCategoryPseudoSelect($(this));
