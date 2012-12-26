0b134730620a687f5ebf8a7d731306e133a153e6
Bongs <saurabh@yournextleap.com>
2012-12-26 16:12:03 +0530

    Init leonidas user for dashboard - optimization

--- app/controllers/recruiters/dashboard_controller.rb
+++ app/controllers/recruiters/dashboard_controller.rb
@@ -1,14 +1,14 @@
 class Recruiters::DashboardController < ApplicationController
+  before_filter :init_leonidas_user, :only => [:dashboard]
+
   layout "recruiters/dashboard"
   def dashboard
   	@jobs = {
       :incomplete => Recruiters::Job.find(:status => Recruiters::Job::STATUSES::INCOMPLETE, :posted_by => current_user.sid).paginate(:page => 1, :per_page => 2),
       :pending => Recruiters::Job.find(:status => Recruiters::Job::STATUSES::PENDING, :posted_by => current_user.sid).paginate(:page => 1, :per_page => 2),
-      :open => Recruiters::Job.open(current_user.leonidas_resource.id, {:page => 1, :per_page => 2}),
-      :closed => Recruiters::Job.closed(current_user.leonidas_resource.id, {:page => 1, :per_page => 2})
+      :open => Recruiters::Job.open(@recruiter, {:page => 1, :per_page => 2}),
+      :closed => Recruiters::Job.closed(@recruiter, {:page => 1, :per_page => 2})
     }
-
-    Rails.logger.ap @jobs
   end
 
   def job_posting
@@ -21,6 +21,11 @@ class Recruiters::DashboardController < ApplicationController
     else
       redirect_to new_recruiters_company_profile_path(:redirect_to => edit_recruiters_jobs_path(:id => @job.uuid, :section => "job-details"))
     end
+  end
 
+  private
+
+  def init_leonidas_user
+    @recruiter = current_user.leonidas_resource
   end
 end
