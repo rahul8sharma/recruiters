1160ab569a95bebcf55e032df351ff25468b1294
Bongs <saurabh@yournextleap.com>
2012-12-26 16:02:57 +0530

    Fixes and removal of unnecessary code

--- app/controllers/recruiters/dashboard_controller.rb
+++ app/controllers/recruiters/dashboard_controller.rb
@@ -4,33 +4,13 @@ class Recruiters::DashboardController < ApplicationController
   	@jobs = {
       :incomplete => Recruiters::Job.find(:status => Recruiters::Job::STATUSES::INCOMPLETE, :posted_by => current_user.sid).paginate(:page => 1, :per_page => 2),
       :pending => Recruiters::Job.find(:status => Recruiters::Job::STATUSES::PENDING, :posted_by => current_user.sid).paginate(:page => 1, :per_page => 2),
-      :open => Recruiters::Job.open(current_user.leonidas_resource.id, {:pagination => {:page => 1, :per_page => 2}}),
-      :closed => Recruiters::Job.closed(current_user.leonidas_resource.id, {:pagination => {:page => 1, :per_page => 2}})
+      :open => Recruiters::Job.open(current_user.leonidas_resource.id, {:page => 1, :per_page => 2}),
+      :closed => Recruiters::Job.closed(current_user.leonidas_resource.id, {:page => 1, :per_page => 2})
     }
 
     Rails.logger.ap @jobs
   end
 
-  # def get_leo_jobs(status)
-  #   jobs = Vger::Spartan::Opus::Recommendation.traverse({
-  #                         :source_nodes => [{
-  #                           "id" => current_user.leonidas_resource.id,
-  #                           "relationship" => "posts_out"
-  #                         }],
-  #                         :options => {
-  #                           "bucket_type" => "Opus::Job",
-  #                           "active" => status
-  #                         },
-  #                         :pagination => {
-  #                           :page => 1,
-  #                           :per_page => 2
-  #                         },
-  #                         :visible => true,
-  #                         :order => "updated_at DESC",
-  #                         :without_cache => true,
-  #                         :plain_fetch => true
-  #           }.merge(options[:pagination] || {}))
-  # end
   def job_posting
     @job = Recruiters::Job.create
     
