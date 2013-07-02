7d1993481700f2bdfb31c26c8929f93009b7aeec
Prashant <prashant.chaudhari89@gmail.com>
2013-07-02 19:32:11 +0530

    removed resource not found rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,11 @@ class ApplicationController < ActionController::Base
 	
 	include Vger::Helpers::AuthenticationHelper
 	
-	rescue_from Faraday::Error::ResourceNotFound, :with => :resource_not_found
-	
 	helper_method :current_user
 	helper_method :can?
   
   protected
   def set_auth_token
   	RequestStore.store[:auth_token] = session[:auth_token] || params[:auth_token]
-  end
-  
-  def resource_not_found(error)
-    redirect_to "/application/thanks/"
   end
 end
