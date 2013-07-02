854dd294a6ac928350fa90fca390cb0bad8591a7
Prashant <prashant.chaudhari89@gmail.com>
2013-07-02 18:29:53 +0530

    added routes and views for JIT urls

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,19 @@
+class CandidatesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def upload_bulk
+  end
+
+  def upload_single
+  end
+
+  def send_test_to_candidates
+  end
+  
+  def send_test_to_employees
+  end
+end
