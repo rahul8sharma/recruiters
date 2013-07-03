7f1e09443f0bf211359a611a824adffc098d4ffe
Chandrakant Zalte <chandrakant@yournextleap.com>
2013-07-03 15:15:27 +0530

    Sass added for reports

--- config/assets_precompile.rb
+++ config/assets_precompile.rb
@@ -1,4 +1,4 @@
 # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
 assets = Rails.application.config.assets
 assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-assets.precompile += %w(shared.css)
+assets.precompile += %w(shared.css reports.css) 
