c0694d5c4c90465361a4f32013d9258560ac1cc2
Prashant <prashant.chaudhari89@gmail.com>
2013-07-11 20:26:40 +0530

    user login pages, 401 handled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
 	def login
 	  redirect_to root_path if current_user
 		if request.post?
-			sign_in(params[:user])
+		  begin
+			  sign_in(params[:user])
+			rescue Faraday::Unauthorized => e
+			  flash[:notice] = e.response[:body][:data][:error]
+			end
 		else
 		end
 	end
