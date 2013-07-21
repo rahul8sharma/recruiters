bc7dff443e4e3e6efff7a21add937915e8571dbf
Prashant Chaudhari <prashant@jombay.com>
2013-07-21 11:13:21 +0530

    adds helpers to get fits

--- app/helpers/suitability/fitment_grades_helper.rb
+++ app/helpers/suitability/fitment_grades_helper.rb
@@ -10,4 +10,16 @@ module Suitability::FitmentGradesHelper
   def get_team_fit_grade(candidate_fit_scores)
     candidate_fit_scores.select{ |fit_score| ["team","people"].any?{|fit_name| fit_score.fit.name.downcase =~ /#{fit_name}/ } }.first.fitment_grade.name
   end
+  
+  def get_job_fit(fits)
+    fits.select{|fit| ["job","role"].any?{|fit_name| fit.name.downcase =~ /#{fit_name}/ }  }.first
+  end
+  
+  def get_company_fit(fits)
+    fits.select{|fit| ["organization","company"].any?{|fit_name| fit.name.downcase =~ /#{fit_name}/ }  }.first
+  end
+  
+  def get_team_fit(fits)
+    fits.select{|fit| ["team","people"].any?{|fit_name| fit.name.downcase =~ /#{fit_name}/ }  }.first
+  end
 end
