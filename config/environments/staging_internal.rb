6ea267adfbe80aa5abf54c618795241e8aad2b09
Prashant Chaudhari <prashant@jombay.com>
2013-07-22 01:10:47 +0530

    fixes asset hosts

--- config/environments/staging_internal.rb
+++ config/environments/staging_internal.rb
@@ -39,6 +39,7 @@ Recruiters::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.action_controller.asset_host = "http://recruiters.internal.ur-nl.com:6080"  
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
