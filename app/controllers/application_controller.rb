c7f9ca88625a4bc08d3e7bfb3dd2e07b7405c0cb
Prashant Chaudhari <prashant@jombay.com>
2013-07-22 18:45:39 +0530

    adds before filter to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,24 @@ class ApplicationController < ActionController::Base
 	
 	include Vger::Helpers::AuthenticationHelper
 	
+	rescue_from Faraday::Unauthorized, :with => :unauthorized
+	rescue_from Faraday::ResourceNotFound, :with => :resource_not_found
+	
 	helper_method :current_user
 	helper_method :can?
   
   protected
   def set_auth_token
   	RequestStore.store[:auth_token] = session[:auth_token] || params[:auth_token]
+  end
+  
+  def unauthorized
+    flash[:notice] = "You must be logged in to visit this page"
+    redirect_to login_path
+  end
+  
+  def resource_not_found
+    flash[:notice] = "This page does not exist"
+    redirect_to root_path
   end
 end
