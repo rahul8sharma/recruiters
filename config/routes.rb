7849496e5d91c2b21f41ef5f2df20f360096bc31
ynl <chandrakant@yournexleap.com>
2013-07-29 12:40:31 +0530

    gradient bg image aaded for JIT pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,21 @@ Recruiters::Application.routes.draw do
   resources :companies, :only => [:index, :show] do
     collection do
       post :import
-  	  get :manage
-      get :destroy_all
-  	  post 'import_from_google_drive'
+      get :manage
+      post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
-
+    
     member do
       get "settings" => "companies#settings", :as => :settings
       get "statistics" => "companies#statistics", :as => :statistics
-      get "settings/company" => "companies#company", :as => :company_settings
+      get "settings/company" => "companies#company", :as => :company_settings                        
       get "settings/account" => "companies#account", :as => :account_settings
-      get "candidates/:candidate_id" => "companies#candidate", :as => :candidate
+      get "candidates/:candidate_id" => "companies#candidate", :as => :candidate                                  
     end
-
+    
     resources :hiring_managers, :only => [:index, :new] do
-    	collection do
+      collection do
         post :import
         get 'assign_jobs' => "hiring_managers#assign_jobs_form", :as => :assign_jobs_form
         post :assign_jobs
@@ -25,17 +24,16 @@ Recruiters::Application.routes.draw do
         post 'export_to_google_drive'
       end
     end
-
+    
     resources :admins, :only => [:index, :new] do
-    	collection do
-    	  get :manage
-        get :destroy_all
-    	  post :import
-    	  post 'import_from_google_drive'
-        post 'export_to_google_drive'
-	    end
+      collection do
+        get :manage
+        post :import
+        post 'import_from_google_drive'
+        post 'export_to_google_drive' 
+      end
     end
-
+    
     resources :assessments, :path => "tests", :except => [:destroy] do
       member do
         get "candidates" => "assessments#candidates", :as => :candidates
@@ -51,7 +49,7 @@ Recruiters::Application.routes.draw do
         put "candidates/:candidate_id/send-reminder" => "assessments#send_reminder", :as => :send_reminder_to_candidate
         get "candidates/:candidate_id" => "assessments#candidate", :as => :candidate
       end
-
+      
       resources :candidates, :except => [:destroy, :show] do
         collection do
           get "upload/bulk" => "candidates#upload_bulk", :as => :bulk_upload
@@ -70,199 +68,176 @@ Recruiters::Application.routes.draw do
         end
       end
     end
-
+    
     resources :jobs, :only => [:show, :index, :new] do
-    	collection do
-    	  get :manage
-        get :destroy_all
-    	  post :import
-    	  post 'import_from_google_drive'
+      collection do
+        get :manage
+        post :import 
+        post 'import_from_google_drive'
         post 'export_to_google_drive'
-	    end
+      end
     end
   end
-
+  
   resources :account_managers, :only => [:index, :new] do
-  	collection do
+    collection do
       post :import
       get 'assign_jobs' => "account_managers#assign_jobs_form", :as => :assign_jobs_form
       post :assign_jobs
       get :manage
-      get :destroy_all
       post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
   end
 
   resources :functional_areas, :only => [:index, :new] do
-	  collection do
-	    post :import
+    collection do 
+      post :import 
       get 'manage'
       post 'import_from_google_drive'
       post 'export_to_google_drive'
-	  end
-  end
-
-  resources :subscriptions, :only => [:index] do
-	  collection do 
-	    post :import 
-      get 'manage'
-      post 'import_from_google_drive'
-      post 'export_to_google_drive'
-	  end
+    end
   end
   
   resources :locations, :only => [:index, :new] do
-	  collection do
-	    post :import
-	    get 'manage'
+    collection do
+      post :import 
+      get 'manage'
       post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
   end
 
   resources :industries, :only => [:index, :new] do
-	  collection do
-	    post :import
-	    get 'manage'
+    collection do
+      post :import 
+      get 'manage'
       post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
   end
 
   resources :job_experiences, :only => [:index, :new] do
-	  collection do
-	    post :import
-	    get 'manage'
+    collection do
+      post :import 
+      get 'manage'
       post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
   end
-
+  
   resources :locations, :only => [:index, :new] do
-	  collection do
-	    post :import
-	    get 'manage'
+    collection do
+      post :import 
+      get 'manage'
       post 'import_from_google_drive'
       post 'export_to_google_drive'
     end
   end
-
+  
   namespace :suitability do
     resources :item_groups do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-	    end
-	  end
-
-	  resources :items, :only => [:index, :new, :show] do
-	  end
-
-	  resources :factor_norm_bucket_descriptions, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :default_factor_norm_ranges, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post 'import_via_s3'
-		    post 'export_to_google_drive'
+      collection do
+        get :manage
+        post :import_from_google_drive
       end
-  	end
-
-  	resources :fitment_grade_mappings, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :fitment_grades, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :fits, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :norm_buckets, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :overall_fitment_grades, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :post_assessment_guidelines, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	resources :recommendations, :only => [:index] do
-		  collection do
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
+    end
+    
+    resources :items, :only => [:index, :new, :show] do
+    end
+  
+    resources :factor_norm_bucket_descriptions, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :default_factor_norm_ranges, :only => [:index] do
+      collection do
+        get :manage
+        post 'import_via_s3'
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :fitment_grade_mappings, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :fitment_grades, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end  
+    
+    resources :fits, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :norm_buckets, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :overall_fitment_grades, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :post_assessment_guidelines, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+    
+    resources :recommendations, :only => [:index] do
+      collection do
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end   
+  
     resources :factors, :only => [:index, :new, :show] do
-		  resources :items do
-			  get 'add_option' => 'items#add_option'
-			  resources :options do
-			  end
-		  end
-		  collection do
-		    post :import
-		    get :manage
-        get :destroy_all
-		    post :import_from_google_drive
-		    post 'export_to_google_drive'
-		  end
-  	end
-
-  	namespace :job do
+      resources :items do
+        get 'add_option' => 'items#add_option'
+        resources :options do
+        end
+      end
+      collection do
+        post :import 
+        get :manage
+        post :import_from_google_drive
+        post 'export_to_google_drive'
+      end
+    end
+    
+    namespace :job do
       resources :factor_norms, :only => [ :index ] do
         collection do
           post :import
           get :edit
-          get :manage
-          get :destroy_all
+          get 'manage'
           post 'import_via_s3'
           post 'export_to_google_drive'
         end
@@ -277,9 +252,9 @@ Recruiters::Application.routes.draw do
   
   get "/users/password/new", :to => "users#forgot_password", :as => :forgot_password
   post "/users/password/create", :to => "users#send_reset_password", :as => :send_reset_password
-	
-	match "/logout", :to => "users#logout", :via => [:get, :delete], :as => :logout
-	match "/sidekiq/upload_reports", :to => "sidekiq#upload_reports"
-	
+  
+  match "/logout", :to => "users#logout", :via => [:get, :delete], :as => :logout
+  match "/sidekiq/upload_reports", :to => "sidekiq#upload_reports"
+  
   root :to => "pages#home"
 end
