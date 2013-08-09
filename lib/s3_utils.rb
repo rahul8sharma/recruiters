3f8590c485bfe5fc408268d67dec5f5b1991b60e
Prashant Chaudhari <prashant@jombay.com>
2013-08-09 15:05:51 +0530

    use aws-sdk interface instead of aws-s3

--- lib/s3_utils.rb
+++ lib/s3_utils.rb
@@ -1,17 +1,27 @@
 module S3Utils
+  def self.s3
+    @@s3 ||= AWS::S3.new(Rails.application.config.s3)
+  end
+  
   def self.ensure_bucket(bucket_name)
-    begin
-      AWS::S3::Bucket.find(bucket_name, :access => :private)
-    rescue AWS::S3::NoSuchBucket => e
-      AWS::S3::Bucket.create(bucket_name)
+    bucket = s3.buckets[bucket_name]
+    unless bucket.exists?
+      bucket = s3.buckets.create(bucket_name)
     end
+    bucket
+  end
+  
+  def self.find(bucket_name,key)
+    s3.buckets[bucket_name].objects[key]
+  end
+  
+  def self.get_url(bucket_name,key)
+    obj = S3Utils.find(bucket_name,key)
+    obj.url_for("get").to_s rescue nil
   end
 
   def self.upload(bucket_name, key, data)
-    S3Utils.ensure_bucket(bucket_name)
-
-    AWS::S3::S3Object.store(key,
-                            data,
-                            bucket_name)
+    bucket = S3Utils.ensure_bucket(bucket_name)
+    obj = bucket.objects.create(key, data)
   end
 end
