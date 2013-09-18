c05fc703f389869c5698c908b8b23c23f232587e
Prashant Chaudhari <prashant@jombay.com>
2013-09-18 15:30:29 +0530

    adds comments

--- app/assets/javascripts/styles.js
+++ app/assets/javascripts/styles.js
@@ -1,3 +1,18 @@
+/*
+  This javascript is added to styles page while creating an assessment
+  Checkboxes to select factors are disabled by default.
+  On document ready  
+  enable all checkboxes for factors
+  Add change even on checkboxes to select factor
+  On checked, enable all input tags under that factor_norm
+  On Uncheck, if factor_norm is new then disable all input tags under that factor_norm, else mark as destroy
+  Also toggle checked class for li tag for that factor_norm
+  
+  For checkboxes to mark a factor as critical,
+  on checked, set value of hidden critical field as on
+  on uncheck, set value of hidden critical field as off
+*/
+
 jQuery(document).ready(function($){
   $("input[name='selected_factor']").removeAttr("disabled");
   $("input[name='selected_factor']").change(function(){
