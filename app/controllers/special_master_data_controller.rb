4b35e7f3c93e04be668801824207ee17d503caec
Prashant Chaudhari <prashant@jombay.com>
2013-12-13 20:54:36 +0530

    special master data controller for master data with industry, function specific data

---
+++ app/controllers/special_master_data_controller.rb
@@ -0,0 +1,27 @@
+class SpecialMasterDataController < MasterDataController 
+  def search_columns
+    [
+      :industry_id,
+      :functional_area_id, 
+      :job_experience_id
+    ]
+  end
+  
+  def select_industry_id
+    industries = [ ["Global",""] ]
+    industries |= Vger::Resources::Industry.all.to_a.collect{|industry| [industry.name, industry.id] }
+    Hash[industries]
+  end
+  
+  def select_functional_area_id
+    functional_areas = [ ["Global",""] ]
+    functional_areas |= Vger::Resources::FunctionalArea.all.to_a.collect{|functional_area| [functional_area.name, functional_area.id] }
+    Hash[functional_areas]
+  end
+  
+  def select_job_experience_id
+    job_experiences = [ ["Global",""] ]
+    job_experiences |= Vger::Resources::JobExperience.all.to_a.collect{|job_exp| [job_exp.display_text, job_exp.id] }
+    Hash[job_experiences]
+  end
+end
