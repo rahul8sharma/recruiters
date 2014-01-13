70b03b6f5a74b56a1f21d84a5e1a5811337bde2a
Prashant Chaudhari <prashant@jombay.com>
2014-01-13 18:43:08 +0530

    upload reports using assessment reports

--- app/controllers/sidekiq_controller.rb
+++ app/controllers/sidekiq_controller.rb
@@ -6,8 +6,8 @@ class SidekiqController < ApplicationController
     render :json => { :status => "Job Started" }
   end
 
-	def upload_reports
-	  reports = Vger::Resources::Suitability::CandidateAssessmentReport.where(
+  def upload_reports
+    reports = Vger::Resources::Suitability::CandidateAssessmentReport.where(
       :query_options => {
         :status =>  Vger::Resources::Suitability::CandidateAssessmentReport::Status::SCORED
       },
@@ -28,27 +28,46 @@ class SidekiqController < ApplicationController
       ReportUploader.perform_async(report_data, RequestStore.store[:auth_token])
     end
     render :json => { :status => "Job Started", :reports => reports }
-	end
-
-	def upload_benchmark_reports
-	  candidate_assessments = Vger::Resources::Suitability::CandidateAssessment.get("/suitability/candidate_assessments/benchmarked?query_options[status]=#{Vger::Resources::Suitability::CandidateAssessment::Status::BENCHMARKED}").to_a
+  end
+  
+  def upload_training_requirements_reports
+    assessment_reports = Vger::Resources::Suitability::AssessmentReport.where(:query_options => { 
+                    :status => Vger::Resources::Suitability::AssessmentReport::Status::UPLOADING, 
+                    :report_type => Vger::Resources::Suitability::Assessment::ReportType::TRAINING_REQUIREMENT
+                  }).all.to_a
     job_ids = []
-    candidate_assessments.map(&:assessment_id).uniq.each do |assessment_id|
+    assessment_reports.each do |assessment_report|
       report_data = {
-        :assessment_id => assessment_id,
-        :candidate_assessment_ids => candidate_assessments.select{|x| x.assessment_id == assessment_id }.map(&:id)
+        :assessment_id => assessment_report.assessment_id,
+        :assessment_report_id => assessment_report.id
+      }
+      job_ids << TrainingRequirementsReportUploader.perform_async(report_data, RequestStore.store[:auth_token])
+    end
+    render :json => { :status => "Job Started", :job_ids => job_ids }
+  end
+
+  def upload_benchmark_reports
+    assessment_reports = Vger::Resources::Suitability::AssessmentReport.where(:query_options => { 
+                    :status => Vger::Resources::Suitability::AssessmentReport::Status::NEW, 
+                    :report_type => Vger::Resources::Suitability::Assessment::ReportType::BENCHMARK
+                  }).all.to_a
+    job_ids = []
+    assessment_reports.each do |assessment_report|
+      report_data = {
+        :assessment_id => assessment_report.assessment_id,
+        :assessment_report_id => assessment_report.id
       }
       job_ids << BenchmarkReportUploader.perform_async(report_data, RequestStore.store[:auth_token])
     end
     render :json => { :status => "Job Started", :job_ids => job_ids }
-	end
+  end
 
-	def regenerate_reports
-	  if params[:args][:assessment_id].present? && params[:args][:email].present?
-	    assessment = Vger::Resources::Suitability::Assessment.regenerate_reports(params)
-	    render :json => { :status => "Job Started", :job_id => assessment.job_id }
-	  else
-	    redirect_to report_management_path, alert: "Please specify email and assessment_id."
-	  end
-	end
+  def regenerate_reports
+    if params[:args][:assessment_id].present? && params[:args][:email].present?
+      assessment = Vger::Resources::Suitability::Assessment.regenerate_reports(params)
+      render :json => { :status => "Job Started", :job_id => assessment.job_id }
+    else
+      redirect_to report_management_path, alert: "Please specify email and assessment_id."
+    end
+  end
 end
