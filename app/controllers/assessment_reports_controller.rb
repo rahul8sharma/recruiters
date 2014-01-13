d8773dd66b447cd90714df87fdb870a93a914b3d
Prashant Chaudhari <prashant@jombay.com>
2014-01-13 19:30:43 +0530

    adds action for competency reports

--- app/controllers/assessment_reports_controller.rb
+++ app/controllers/assessment_reports_controller.rb
@@ -4,21 +4,27 @@ class AssessmentReportsController < ApplicationController
   before_filter :check_superadmin, :only => [ :manage, :assessment_report ]
   
   def training_requirements_report
-    @assessment = Vger::Resources::Suitability::Assessment.find(params[:id], methods: [:training_requirement_report])
+    @assessment = Vger::Resources::Suitability::Assessment.find(params[:id])
+    @assessment_report = Vger::Resources::Suitability::AssessmentReport.where(:query_options => { 
+                            assessment_id: params[:id], 
+                            report_type: Vger::Resources::Suitability::Assessment::ReportType::TRAINING_REQUIREMENT,
+                            status: Vger::Resources::Suitability::AssessmentReport::Status::UPLOADED
+                          }).all.to_a.first
+    if !@assessment_report                      
+      redirect_to company_assessment_path(:company_id => params[:company_id], :id => params[:id]), alert: "Assessment Report not found!"
+      return
+    end
     if request.format == "application/pdf"
       @view_mode = "pdf"
     else  
       @view_mode = "html"
     end
     respond_to do |format|
-      format.html { render :template => "assessment_reports/training_requirements_report" }
+      format.html { 
+        render template: "assessment_reports/training_requirements_report"
+      }
       format.pdf { 
         render pdf: "training_requirements_report_#{params[:id]}.pdf",
-        header: { 
-          :html => {
-            template: "shared/reports/_training_requirements_report_header.html.haml"
-          }
-        },
         footer: {
           :html => {
             template: "shared/reports/_report_footer.html.haml"
@@ -47,11 +53,6 @@ class AssessmentReportsController < ApplicationController
       format.html { render :template => "assessment_reports/benchmark_report" }
       format.pdf { 
         render pdf: "report_#{params[:id]}.pdf",
-        header: { 
-          :html => {
-            template: "shared/reports/_benchmark_report_header.html.haml"
-          }
-        },
         footer: {
           :html => {
             template: "shared/reports/_report_footer.html.haml"
@@ -111,11 +112,6 @@ class AssessmentReportsController < ApplicationController
       format.html { render :template => "assessment_reports/#{template}" }
       format.pdf { 
         render pdf: "report_#{params[:id]}.pdf",
-        header: { 
-          :html => {
-            template: "shared/reports/_report_header.html.haml"
-          }
-        },
         footer: {
           :html => {
             template: "shared/reports/_report_footer.html.haml"
