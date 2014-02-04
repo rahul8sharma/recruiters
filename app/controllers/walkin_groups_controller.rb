40d46c432f699b975422ae3359e799897a92bbd8
Prashant Chaudhari <prashant@jombay.com>
2014-02-04 16:36:25 +0530

    renamed variable names

--- app/controllers/walkin_groups_controller.rb
+++ app/controllers/walkin_groups_controller.rb
@@ -2,12 +2,12 @@ class WalkinGroupsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :get_company
   
-  before_filter :get_assessment_group, :except => [ :index, :create, :new ]
+  before_filter :get_walkin_group, :except => [ :index, :create, :new ]
   
   layout "walk_ins"
   
   def index
-    @assessment_groups = Vger::Resources::Suitability::WalkinGroup.where(:query_options => {  
+    @walkin_groups = Vger::Resources::Suitability::WalkinGroup.where(:query_options => {  
                             company_id: @company.id
                           },
                           methods: [:url],
@@ -21,44 +21,44 @@ class WalkinGroupsController < ApplicationController
   
   def new
     @assessments = Vger::Resources::Suitability::Assessment.where(:query_options => { :company_id => @company.id }, select: ["name","id"], order: ["created_at DESC"]).all
-    @assessment_group = Vger::Resources::Suitability::WalkinGroup.new(:company_id => @company.id, :expires_on => Time.now + 24.hours)
+    @walkin_group = Vger::Resources::Suitability::WalkinGroup.new(:company_id => @company.id, :expires_on => Time.now + 24.hours)
   end
   
   def create
     @assessments = Vger::Resources::Suitability::Assessment.where(:query_options => { :company_id => @company.id }, select: ["name","id"]).all
-    params[:assessment_group][:assessment_hash].reject!{|assessment_id, assessment_data| assessment_data["enabled"] != "true" }
-    @assessment_group = Vger::Resources::Suitability::WalkinGroup.new(params[:assessment_group])
-    if @assessment_group.assessment_hash.present? && @assessment_group.save
-      redirect_to customize_company_assessment_group_path(@company, @assessment_group)
+    params[:walkin_group][:assessment_hash].reject!{|assessment_id, assessment_data| assessment_data["enabled"] != "true" }
+    @walkin_group = Vger::Resources::Suitability::WalkinGroup.new(params[:walkin_group])
+    if @walkin_group.assessment_hash.present? && @walkin_group.save
+      redirect_to customize_company_walkin_group_path(@company, @walkin_group)
     else
-      @assessment_group.error_messages ||= []
-      @assessment_group.error_messages << "Please select at least one assessment." if !@assessment_group.assessment_hash.present?
-      flash[:error] = @assessment_group.error_messages.join("<br/>")
+      @walkin_group.error_messages ||= []
+      @walkin_group.error_messages << "Please select at least one assessment." if !@walkin_group.assessment_hash.present?
+      flash[:error] = @walkin_group.error_messages.join("<br/>")
       render :action => :new
     end
   end
   
   def customize
     if request.put?
-      @assessment_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@assessment_group.id, params[:assessment_group])
-      redirect_to summary_company_assessment_group_path(@company, @assessment_group)
+      @walkin_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@walkin_group.id, params[:walkin_group])
+      redirect_to summary_company_walkin_group_path(@company, @walkin_group)
     else
     end               
   end
   
   def update
-    @assessment_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@assessment_group.id, params[:assessment_group])
-    redirect_to company_assessment_group_path(@company, @assessment_group)
+    @walkin_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@walkin_group.id, params[:walkin_group])
+    redirect_to company_walkin_group_path(@company, @walkin_group)
   end
   
   def summary
   end
   
   def expire
-    @assessment_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@assessment_group.id, {
+    @walkin_group = Vger::Resources::Suitability::WalkinGroup.save_existing(@walkin_group.id, {
       :expires_on => Time.now
     })
-    redirect_to company_assessment_group_path(@company, @assessment_group), notice: "This Walk-In page is marked as expired now."
+    redirect_to company_walkin_group_path(@company, @walkin_group), notice: "This Walk-In page is marked as expired now."
   end
   
   protected
@@ -73,12 +73,12 @@ class WalkinGroupsController < ApplicationController
     @company = Vger::Resources::Company.find(params[:company_id], :methods => methods)
   end
   
-  def get_assessment_group  
-    @assessment_group = Vger::Resources::Suitability::WalkinGroup.find(params[:id], methods: [:url])
-    @assessment_group.expires_on = DateTime.parse(@assessment_group.expires_on) if @assessment_group.expires_on.present?
+  def get_walkin_group  
+    @walkin_group = Vger::Resources::Suitability::WalkinGroup.find(params[:id], methods: [:url])
+    @walkin_group.expires_on = DateTime.parse(@walkin_group.expires_on) if @walkin_group.expires_on.present?
     @assessments = Vger::Resources::Suitability::Assessment.where(:query_options => { 
                       company_id: @company.id, 
-                      id: @assessment_group.assessment_hash.keys
+                      id: @walkin_group.assessment_hash.keys
                     }, 
                     select: ["name","id"]
                    ).all.to_a
