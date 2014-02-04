fce78b04641a7de87ef82026de7dce97fed52efd
Prashant Chaudhari <prashant@jombay.com>
2014-02-04 16:37:02 +0530

    training requirement routes and views

---
+++ app/controllers/training_requirement_groups_controller.rb
@@ -0,0 +1,24 @@
+class TrainingRequirementGroupsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :get_company
+  
+  layout "training_requirement_groups"
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  protected
+  
+  def get_company
+    methods = []
+    if ["show","index", "training_requirements"].include? params[:action]
+      if Rails.application.config.statistics[:load_assessmentwise_statistics]
+        methods << :assessmentwise_statistics
+      end
+    end
+    @company = Vger::Resources::Company.find(params[:company_id], :methods => methods)
+  end
+end
