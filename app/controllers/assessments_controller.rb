2ba6d97eac7cdf3d4d9579109111e5b94b85e5b5
Prashant Chaudhari <prashant@jombay.com>
2014-04-15 19:41:15 +0530

    generalized assessments controller

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,25 +2,15 @@ class AssessmentsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :get_assessment, :except => [:index]
   before_filter :get_meta_data, :only => [:new, :norms, :competency_norms]
-  before_filter :get_company
-  
   
   layout "tests"
-  def reports
-    @candidate_assessments = Vger::Resources::Suitability::CandidateAssessment.where(
-    :assessment_id => @assessment.id,
-    :joins => :candidate_assessment_reports,
-    :include => [:candidate_assessment_reports, :candidate],
-    :query_options => {
-      "suitability_candidate_assessment_reports.status" => Vger::Resources::Suitability::CandidateAssessmentReport::Status::UPLOADED
-    },
-    :page => params[:page],
-    :per=>10).all
+  
+  def api_resource
+    Vger::Resources::Suitability::Assessment
   end
-
   
   def show
-    @assessment = Vger::Resources::Suitability::CustomAssessment.find(params[:id])
+    @assessment = api_resource.find(params[:id])
     @assessment_factor_norms = @assessment.job_assessment_factor_norms.where(:include  => [:functional_area, :industry, :job_experience, :from_norm_bucket, :to_norm_bucket], :include => { :factor => { :methods => [:type, :direct_predictor_ids] } }).all.to_a
     
     direct_predictor_parent_ids = @assessment_factor_norms.collect{ |factor_norm| factor_norm.factor.direct_predictor_ids.present? ? factor_norm.factor_id : nil }.compact.uniq
@@ -29,36 +19,7 @@ class AssessmentsController < ApplicationController
     alarm_factor_norms = @assessment_factor_norms.select{ |factor_norm| factor_norm.factor.type == "Suitability::AlarmFactor" }.uniq
     @assessment_factor_norms = @assessment_factor_norms - direct_predictor_norms - lie_detector_norms
     @other_norms = direct_predictor_norms
-  end
-
-  def competencies
-    @global_competencies = Vger::Resources::Suitability::Competency.global(:query_options => {:active => true}, :methods => [:factor_names], :order => ["name ASC"]).to_a
-    @local_competencies = Vger::Resources::Suitability::Competency.where(:query_options => { "companies_competencies.company_id" => @company.id, :active => true }, :methods => [:factor_names], :order => ["name ASC"], :joins => "companies").all.to_a
-    if request.get?
-    else
-      params[:assessment] ||= {}
-      params[:assessment][:competencies] ||= []
-      if params[:assessment][:competencies].blank?
-        flash[:error] = "Competencies to be measured must be selected before proceeding!"
-        return
-      else
-        selected_competency_ids = params[:assessment][:competencies].map(&:to_i)
-        selected_competencies = Hash[params[:assessment][:competency_order].select{|competency_id,order| selected_competency_ids.include?(competency_id.to_i) }]
-        competency_order = Hash[selected_competencies.map{|competency_id,order| [competency_id.to_i,order.to_i] }]
-        competency_order = Hash[competency_order.sort_by{|competency_id, order| order }]
-        if competency_order.values.size != competency_order.values.uniq.size
-          flash[:error] = "Competencies should have unique order!"
-          return
-        end
-        ordered_competencies = competency_order.keys.select{|competency_id| selected_competency_ids.include?(competency_id) }
-        @assessment = Vger::Resources::Suitability::CustomAssessment.save_existing(@assessment.id, { competency_order: ordered_competencies })
-        if @assessment.error_messages.blank?
-          redirect_to competency_norms_company_assessment_path(:company_id => params[:company_id], :id => @assessment.id)          
-        else
-          flash[:error] = @assessment.error_messages.join("<br/>")
-        end
-      end  
-    end
+    @norm_buckets = Hash[Vger::Resources::Suitability::NormBucket.all.to_a.map{|norm_bucket| [norm_bucket.id,norm_bucket] }]
   end
 
   def competency_norms
@@ -79,7 +40,7 @@ class AssessmentsController < ApplicationController
     get_norms
     if request.get?
       if params[:assessment]
-        @assessment = Vger::Resources::Suitability::CustomAssessment.save_existing(@assessment.id, params[:assessment])
+        @assessment = api_resource.save_existing(@assessment.id, params[:assessment])
       end
     elsif request.put?  
       store_assessment_factor_norms
@@ -87,91 +48,14 @@ class AssessmentsController < ApplicationController
     end
   end
   
-  # fetches styles data
-  # GET : renders styles
-  # PUT : updates assessment and redirects to add_candidates
-  def styles
-    get_styles
-    if request.put?  
-      params[:assessment] ||= {}
-      @assessment = Vger::Resources::Suitability::CustomAssessment.save_existing(@assessment.id, params[:assessment])
-      if @assessment.error_messages.blank?
-        create_or_update_set
-        redirect_to add_candidates_company_assessment_path(:company_id => params[:company_id], :id => @assessment.id) and return
-      else
-        @assessment.error_messages << @assessment.errors.full_messages.dup
-        @assessment.error_messages.flatten!
-        flash[:error] = @assessment.error_messages.join("<br/>")
-      end
-    end  
-  end
   
-  # GET /assessments
-  def index
-    @assessments = Vger::Resources::Suitability::CustomAssessment.where(:query_options => { :company_id => params[:company_id], :assessment_type => ["fit","competency"] }, :order => "created_at DESC", :page => params[:page], :per => 15)
-  end
   
   # GET /assessments/new
   # GET /assessments/new.json
   def new
   end
 
-  # POST /assessments
-  # POST /assessments.json
-  # POST creates assessment and redirects to norms page
-  def create
-    #default_norm_bucket_ranges = get_default_norm_bucket_ranges
-    respond_to do |format|
-      if @assessment.valid? and @assessment.save
-        redirect_path = if @assessment.assessment_type == Vger::Resources::Suitability::CustomAssessment::AssessmentType::FIT
-          norms_company_assessment_path(:company_id => params[:company_id], :id => @assessment.id)
-        elsif @assessment.assessment_type == Vger::Resources::Suitability::CustomAssessment::AssessmentType::COMPETENCY
-          competencies_company_assessment_path(:company_id => params[:company_id], :id => @assessment.id)
-        elsif  @assessment.assessment_type == Vger::Resources::Suitability::CustomAssessment::AssessmentType::BENCHMARK
-          norms_company_benchmark_path(:company_id => params[:company_id], :id => @assessment.id)
-        end
-        format.html { redirect_to redirect_path }
-      else
-        get_meta_data
-        flash[:error] ||= @assessment.errors.values.flatten.join(",") rescue ""
-        format.html { render action: "new" }
-      end
-    end
-  end
- 
   protected
-  
-  # fetches assessment if id is present in params
-  # creates new assessment otherwise
-  def get_assessment
-    if params[:id].present?
-      @assessment = Vger::Resources::Suitability::CustomAssessment.find(params[:id], :include => [:functional_area, :industry, :job_experience], :methods => [:competency_ids])
-      if(@assessment.company_id.to_i == params[:company_id].to_i)
-      else
-        redirect_to root_path, alert: "Page you are looking for doesn't exist."
-      end
-    else
-      @assessment = Vger::Resources::Suitability::CustomAssessment.new(params[:assessment])
-      @assessment.report_types ||= []
-      assessment_type = if params[:fit].present?
-         Vger::Resources::Suitability::CustomAssessment::AssessmentType::FIT
-      elsif params[:competency].present?  
-        Vger::Resources::Suitability::CustomAssessment::AssessmentType::COMPETENCY
-      else
-        @assessment.report_types << Vger::Resources::Suitability::CustomAssessment::ReportType::BENCHMARK
-        Vger::Resources::Suitability::CustomAssessment::AssessmentType::BENCHMARK
-      end
-      if params[:enable_training_requirements_report].present?
-        @assessment.report_types << Vger::Resources::Suitability::CustomAssessment::ReportType::TRAINING_REQUIREMENT
-      end
-      @assessment.report_types.uniq!
-      @assessment.assessment_type = assessment_type
-      @assessment.assessable_type = "Company"
-      @assessment.assessable_id = params[:company_id]
-      @assessment.company_id = params[:company_id]
-    end
-  end
-  
   # fetches meta data for new assessment and adding norms to existing assessment 
   def get_meta_data
     factors = Vger::Resources::Suitability::Factor.active.where(:methods => [:type]).all.to_a
@@ -362,46 +246,6 @@ class AssessmentsController < ApplicationController
                                     where(:query_options => query_options).all.to_a
     end
     default_norm_bucket_ranges       
-  end
-  
-  def get_company
-    methods = []
-    if ["show","index", "training_requirements"].include? params[:action]
-      if Rails.application.config.statistics[:load_assessmentwise_statistics]
-        methods << :assessmentwise_statistics
-      end
-    end
-    @company = Vger::Resources::Company.find(params[:company_id], :methods => methods)
-  end
-  
-  def store_assessment_factor_norms
-    params[:assessment][:job_assessment_factor_norms_attributes] ||= {}
-    if !params[:assessment][:job_assessment_factor_norms_attributes].select{|index,data| data[:_destroy] != "true" }.present?
-      flash[:error] = "Please select at least one factor to proceed."
-      return
-    else
-      params[:assessment][:job_assessment_factor_norms_attributes].each do |index, factor_norms_attributes|
-        norm_buckets_by_id = Hash[@norm_buckets.collect{|norm_bucket| [norm_bucket.id,norm_bucket] }]
-        if factor_norms_attributes[:from_norm_bucket_id]
-          from_weight = norm_buckets_by_id[factor_norms_attributes[:from_norm_bucket_id].to_i].weight
-          to_weight = norm_buckets_by_id[factor_norms_attributes[:to_norm_bucket_id].to_i].weight
-          if from_weight > to_weight
-            flash[:error] = "Upper Limit in the Desired/Acceptable Score Range must be of a greater value than the selected Lower Limit."
-            return
-          end
-        end
-      end
-      @assessment = Vger::Resources::Suitability::CustomAssessment.save_existing(@assessment.id, params[:assessment])
-      if @assessment.error_messages.blank?
-        if @assessment.assessment_type == Vger::Resources::Suitability::CustomAssessment::AssessmentType::BENCHMARK
-          redirect_to add_candidates_company_benchmark_path(:company_id => params[:company_id], :id => @assessment.id)          
-        else
-          redirect_to styles_company_assessment_path(:company_id => params[:company_id], :id => @assessment.id)          
-        end
-      else
-        flash[:error] = @assessment.error_messages.join("<br/>")
-      end
-    end 
   end
   
   def create_or_update_set
