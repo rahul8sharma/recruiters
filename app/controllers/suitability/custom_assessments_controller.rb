f96d3872070f3600bcbad192e77d234c9332b8d6
abhishekverma <abhishekverma.xander@gmail.com>
2014-06-13 15:12:27 +0530

    User Specific Norms Validation

--- app/controllers/suitability/custom_assessments_controller.rb
+++ app/controllers/suitability/custom_assessments_controller.rb
@@ -27,15 +27,15 @@ class Suitability::CustomAssessmentsController < AssessmentsController
       end
     elsif request.put?
       params[:assessment][:job_assessment_factor_norms_attributes] ||= {}
-      traits_range_min = Rails.application.config.validators["traits_range"]["min"]
-      # traits_range_max = Rails.application.config.validators["traits_range"]["max"]      
+      traits_range = Rails.application.config.validators["traits_range"][current_user.type]
       selected_traits_size = params[:assessment][:job_assessment_factor_norms_attributes].select{|index,data| data[:_destroy] != "true" }.keys.size
       
-      if selected_traits_size >= traits_range_min
+      
+      if selected_traits_size >= traits_range["min"] && selected_traits_size <= traits_range["max"]
         store_assessment_factor_norms
       else
-        if selected_traits_size < traits_range_min
-          flash[:error] = "Please select at least #{traits_range_min} traits to create the assessment."
+        if selected_traits_size < traits_range["min"] || selected_traits_size > traits_range["max"]
+          flash[:error] = "Please select at least #{traits_range["min"]} traits to create the assessment.Maximum traits allowed in an assessment is #{traits_range["max"]}"
         # elsif selected_traits_size > traits_range_max
         #   flash[:error] = "Maximum traits allowed in an assessment is 18 to ensure optimum assessment experience. Please deselect a few traits to Proceed."
         end
