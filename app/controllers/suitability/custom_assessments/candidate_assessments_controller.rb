2c934d8ce5e23de506c1c483160f15506950bce8
abhishekverma <abhishekverma.xander@gmail.com>
2014-07-07 12:50:54 +0530

    Release 1.29 - Feature - Cell -8A
    Dont have a default selection between development and hiring while
    creating an assessment. Need to add an error message as well.
    
    "Leave both the radio buttons deselected.
    
    If a user tries to proceed without selecting anything, error message:
    ""Please select the purpose of assessing these Assessment Takers before
    proceeding!""

--- app/controllers/suitability/custom_assessments/candidate_assessments_controller.rb
+++ app/controllers/suitability/custom_assessments/candidate_assessments_controller.rb
@@ -41,49 +41,53 @@ class Suitability::CustomAssessments::CandidateAssessmentsController < Applicati
     params[:candidates] ||= {}
     params[:candidates].reject!{|key,data| data[:email].blank? && data[:name].blank?}
     #params[:candidates] = Hash[params[:candidates].collect{|key,data| [data[:email], data] }]
-    params[:candidate_stage] ||= Vger::Resources::Candidate::Stage::EMPLOYED
     params[:upload_method] ||= "manual"
     @functional_areas = Vger::Resources::FunctionalArea.active.all.to_a
     @errors = {}
     assessment_factor_norms = @assessment.job_assessment_factor_norms.all.to_a
     if request.put?
-      candidates = {}
-      if params[:candidates].empty? 
-        flash[:error] = "Please add at least 1 Assessment Taker to send the assessment. You may also select 'Add Assessment Takers Later' to save the assessment and return to the Assessment Listings."
+      if params[:candidate_stage].empty?
+        flash[:error] = "Please select the purpose of assessing these Assessment Takers before proceeding!"
         render :action => :add_candidates and return
-      end
-      params[:candidates].each do |key,candidate_data|
-        if candidate_data[:email].present?
-          candidate = Vger::Resources::Candidate.where(:query_options => { :email => candidate_data[:email] }).all[0]
+      else        
+        candidates = {}
+        if params[:candidates].empty? 
+          flash[:error] = "Please add at least 1 Assessment Taker to send the assessment. You may also select 'Add Assessment Takers Later' to save the assessment and return to the Assessment Listings."
+          render :action => :add_candidates and return
         end
-        @errors[key] ||= []
-        if candidate
-          candidate_data[:id] = candidate.id
-          candidates[candidate.id] = candidate_data
-          attributes_to_update = candidate_data.dup
-          attributes_to_update.each { |attribute,value| attributes_to_update.delete(attribute) unless candidate.send(attribute).blank? }
-          Vger::Resources::Candidate.save_existing(candidate.id, attributes_to_update)
-        else
-          candidate = Vger::Resources::Candidate.create(candidate_data)
-          if candidate.error_messages.present?
-            @errors[key] |= candidate.error_messages
-          else
+        params[:candidates].each do |key,candidate_data|
+          if candidate_data[:email].present?
+            candidate = Vger::Resources::Candidate.where(:query_options => { :email => candidate_data[:email] }).all[0]
+          end
+          @errors[key] ||= []
+          if candidate
             candidate_data[:id] = candidate.id
             candidates[candidate.id] = candidate_data
-          end
-        end  
-      end
-      unless @errors.values.flatten.empty?
-        #flash[:error] = "Errors in provided data: <br/>".html_safe
-        flash[:error] = @errors.map.with_index do |(candidate_name, candidate_errors), index| 
-          if candidate_errors.present?
-            ["#{candidate_errors.join("<br/>")}"]
+            attributes_to_update = candidate_data.dup
+            attributes_to_update.each { |attribute,value| attributes_to_update.delete(attribute) unless candidate.send(attribute).blank? }
+            Vger::Resources::Candidate.save_existing(candidate.id, attributes_to_update)
+          else
+            candidate = Vger::Resources::Candidate.create(candidate_data)
+            if candidate.error_messages.present?
+              @errors[key] |= candidate.error_messages
+            else
+              candidate_data[:id] = candidate.id
+              candidates[candidate.id] = candidate_data
+            end
           end  
-        end.compact.uniq.join("<br/>").html_safe
-        render :action => :add_candidates and return
+        end
+        unless @errors.values.flatten.empty?
+          #flash[:error] = "Errors in provided data: <br/>".html_safe
+          flash[:error] = @errors.map.with_index do |(candidate_name, candidate_errors), index| 
+            if candidate_errors.present?
+              ["#{candidate_errors.join("<br/>")}"]
+            end  
+          end.compact.uniq.join("<br/>").html_safe
+          render :action => :add_candidates and return
+        end
+        params[:candidates] = candidates
+        render :action => :send_test_to_candidates
       end
-      params[:candidates] = candidates
-      render :action => :send_test_to_candidates
     else
       if assessment_factor_norms.size <= 1
         flash[:error] = "You need to select traits before sending an assessment. Please select traits from below."
