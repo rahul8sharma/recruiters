513d9b7e8f9e6f2e0b2608efd1905b1899dfd60d
abhishekverma <abhishekverma.xander@gmail.com>
2014-08-28 18:23:32 +0530

    Reports Intergration
    Missing Values in Reports Hash
    1) Report Generation date
    2) Traits Description
    3) Average Rating by Individual Stakeholders

--- app/controllers/mrf/assessments/reports_controller.rb
+++ app/controllers/mrf/assessments/reports_controller.rb
@@ -3,6 +3,9 @@ class Mrf::Assessments::ReportsController < ApplicationController
   layout "reports_360"
   
   def report
+    @norm_buckets = Vger::Resources::Suitability::NormBucket.all
+    @report = Vger::Resources::Mrf::Report.where(:query_options =>{:candidate_id => params[:candidate_id], :assessment_id => params[:id]}).all.to_a.first
+    Rails.logger.debug(@report.report_data)
   end
 
 end
