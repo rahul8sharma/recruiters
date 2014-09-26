7ab8cf984fe772ab03c107187e2a15cde1a3c9e0
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2014-09-26 18:42:57 +0530

    Routes and MasterData pages and controllers

---
+++ app/controllers/mrf/default_trait_norm_ranges_controller.rb
@@ -0,0 +1,15 @@
+class Mrf::DefaultTraitNormRangesController < MasterDataController
+
+def api_resource
+    Vger::Resources::Mrf::DefaultTraitNormRange
+  end
+
+  def import_from
+    "import_from_google_drive"
+  end
+
+  def index_columns
+    [:id,:trait_id,:trait_type,:from_norm_bucket_id,:to_norm_bucket_id]
+  end
+
+end
