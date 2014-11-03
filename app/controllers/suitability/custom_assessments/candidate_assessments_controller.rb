e843bef866c67235a02164e02baab25b90d25d5d
Prashant Chaudhari <prashant@jombay.com>
2014-11-03 18:56:05 +0530

    set template id only if present

--- app/controllers/suitability/custom_assessments/candidate_assessments_controller.rb
+++ app/controllers/suitability/custom_assessments/candidate_assessments_controller.rb
@@ -135,7 +135,7 @@ class Suitability::CustomAssessments::CandidateAssessmentsController < Applicati
                     :worksheets => [{
                       :functional_area_id => params[:functional_area_id],
                       :candidate_stage => params[:candidate_stage],
-                      :template_id => params[:template_id].to_i,
+                      :template_id => params[:template_id].present? ? params[:template_id].to_i : nil, 
                       :file => "BulkUpload.csv",
                       :bucket => params[:s3_bucket],
                       :key => params[:s3_key]
