bb350e4e629682b9309bc8c104191b161230c9a4
Prashant Chaudhari <prashant@jombay.com>
2014-11-04 17:12:17 +0530

    add subject and from

--- app/assets/javascripts/template.js
+++ app/assets/javascripts/template.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function($){ 
+  var currentElement = null;
+  $("#template_body, #template_subject, #template_from").click(function(){
+    currentElement = $(this);
+  });
+  
   $(".template_variable_link").click(function(){
-    $("#template_body").insertAtCaret("<$"+$(this).attr("template_variable_name")+"$>");
+    currentElement.insertAtCaret("<$"+$(this).attr("template_variable_name")+"$>");
   });
 });
