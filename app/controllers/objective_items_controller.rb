0dbbfd3534bf3010907efd69e35d944692fd169a
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2014-12-02 16:06:43 +0530

    Minor fixes

--- app/controllers/objective_items_controller.rb
+++ app/controllers/objective_items_controller.rb
@@ -13,41 +13,41 @@ class ObjectiveItemsController < MasterDataController
 
   def import_with_options_from_google_drive
     errors = Vger::Resources::ObjectiveItem.import_with_options_from_google_drive(params[:item])
-    redirect_to manage_mrf_items_path, notice: "Import operation queued. Email notification should arrive as soon as the import is complete."
+    redirect_to manage_objective_items_path, notice: "Import operation queued. Email notification should arrive as soon as the import is complete."
   end
 
-    # GET /items/:id
-    # GET /items/:id.json
-    def show
-      @item= Vger::Resources::ObjectiveItem.find(params[:id], :include => [ :options ])
-      respond_to do |format|
-        format.html
-      end
+  # GET /items/:id
+  # GET /items/:id.json
+  def show
+    @item= Vger::Resources::ObjectiveItem.find(params[:id], :include => [ :options ])
+    respond_to do |format|
+      format.html
     end
+  end
 
-    # GET /items/:id
-    # GET /items/:id.json
-    def edit
-      @item= Vger::Resources::ObjectiveItem.find(params[:id])
-      respond_to do |format|
-        format.html
-      end
+  # GET /items/:id
+  # GET /items/:id.json
+  def edit
+    @item= Vger::Resources::ObjectiveItem.find(params[:id])
+    respond_to do |format|
+      format.html
     end
+  end
 
-    # PUT /items/:id
-    # PUT /items/:id.json
-    def update
-      @item = Vger::Resources::ObjectiveItem.find(params[:id])
-      respond_to do |format|
-        if @item.class.save_existing(params[:id], params[:item])
-          format.html { redirect_to objective_item_path(params[:id]), notice: 'Objective Item was successfully updated.' }
-          format.json { render json: @item, status: :created, location: @item}
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @item.errors, status: :unprocessable_entity }
-        end
+  # PUT /items/:id
+  # PUT /items/:id.json
+  def update
+    @item = Vger::Resources::ObjectiveItem.find(params[:id])
+    respond_to do |format|
+      if @item.class.save_existing(params[:id], params[:item])
+        format.html { redirect_to objective_item_path(params[:id]), notice: 'Objective Item was successfully updated.' }
+        format.json { render json: @item, status: :created, location: @item}
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
       end
     end
+  end
 
   def import_from
     "import_from_google_drive"
