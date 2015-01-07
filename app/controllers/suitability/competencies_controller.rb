6b5dd2b20e714d2bf3bdfe749897ba524ddeb87f
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2015-01-07 19:35:04 +0530

    One leg of implementation done

--- app/controllers/suitability/competencies_controller.rb
+++ app/controllers/suitability/competencies_controller.rb
@@ -19,6 +19,8 @@ class Suitability::CompetenciesController < MasterDataController
   end
 
   def new
+    params[:forms] ||= 1
+    @forms = params[:forms].to_i
     params[:competency] ||= {}
     @competency = Vger::Resources::Suitability::Competency.new
     respond_to do |format|
@@ -30,29 +32,32 @@ class Suitability::CompetenciesController < MasterDataController
   # POST /suitabilty/competencies.json
   # POST creates competency
   def create
-    company_ids = params[:company_ids].to_s.split(",").map(&:to_i)
-    params[:factor_ids] ||= {}
-    params[:mrf_trait_ids] ||= {}
-    params[:functional_trait_ids] ||= {}
 
-    factor_ids = params[:factor_ids]\
-      .collect { |index,factor_hash| factor_hash.keys}\
-      .flatten
-    mrf_trait_ids = params[:mrf_trait_ids]\
-      .collect { |index, factor_hash| factor_hash.keys}\
-      .flatten
-    functional_trait_ids = params[:functional_trait_ids]\
-      .collect { |index, factor_hash| factor_hash.keys}\
-      .flatten
+    if ((params[:factor_ids] || params[:mrf_trait_ids] || params[:functional_trait_ids]) && (params[:competency][:name].present?))
+      company_ids = params[:company_ids].to_s.split(",").map(&:to_i)
+      params[:factor_ids] ||= {}
+      params[:mrf_trait_ids] ||= {}
+      params[:functional_trait_ids] ||= {}
 
+      factor_ids = params[:factor_ids]\
+        .collect { |index,factor_hash| factor_hash.keys}\
+        .flatten
+      mrf_trait_ids = params[:mrf_trait_ids]\
+        .collect { |index, factor_hash| factor_hash.keys}\
+        .flatten
+      functional_trait_ids = params[:functional_trait_ids]\
+        .collect { |index, factor_hash| factor_hash.keys}\
+        .flatten
+      params[:competency][:factor_ids] = factor_ids
+      params[:competency][:mrf_trait_ids] = mrf_trait_ids
+      params[:competency][:functional_trait_ids] = functional_trait_ids
+      params[:competency][:company_ids] = company_ids
 
-    params[:competency][:factor_ids] = factor_ids
-    params[:competency][:mrf_trait_ids] = mrf_trait_ids
-    params[:competency][:functional_trait_ids] = functional_trait_ids
-    params[:competency][:company_ids] = company_ids
-
-    @competency = Vger::Resources::Suitability::Competency.create(params[:competency])
-    redirect_to self.send("#{resource_name.singularize}_path",@competency)
+      @competency = Vger::Resources::Suitability::Competency.create(params[:competency])
+      redirect_to self.send("#{resource_name.singularize}_path",@competency)
+    else
+      flash[:error] = "Either competency name is missing or factor ids are missing. Please Check"
+    end
   end
 
   def edit
