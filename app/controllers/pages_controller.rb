f44b4fb2d2a316d3e1698d17f3ca01b4b6b68f46
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2015-01-08 13:12:15 +0530

    Assessment Setup and candidate assessment setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,21 +32,54 @@ class PagesController < ApplicationController
           competency_ids = @assessment.competency_ids
           @competencies = Vger::Resources::Suitability::Competency.find(competency_ids)
         end
-        @norm_buckets = Vger::Resources::Suitability::NormBucket.where(:order => "weight ASC").all
-        @objective_items = Vger::Resources::ObjectiveItem.where(:query_options => { :active => true}, :include => [ :options ]).all.to_a
-        objective_ids = @objective_items.map(&:id)
-        # @objective_options = Vger::Resources::ObjectiveOption.where(:query_options => {:objective_item_ids => objective_ids})
-        # @objective_options = @objective_options.group_by { |option| option.objective_item_id}
-        @subjective_items = Vger::Resources::SubjectiveItem.active.all.to_a
-        #Show candidate details
-        @candidate_details = params[:report]
-        #show flags
       else
-
+        params[:assessment][:is_jombay_pearson_test] = params[:assessment][:is_jombay_pearson_test] == "Yes"
         #Create New Custom Assessment
+        @assessment = Vger::Resources::Suitability::CustomAssessment.create(params[:assessment].except!(:assessment_id))
         #Figure out Competency Norms Assessment Norms Logic
         #show flags
         #Show candidate details
+      end
+      @norm_buckets = Vger::Resources::Suitability::NormBucket.where(:order => "weight ASC").all
+      @objective_items = Vger::Resources::ObjectiveItem.where(:query_options => { :active => true}, :include => [ :options ]).all.to_a
+      objective_ids = @objective_items.map(&:id)
+      # @objective_options = Vger::Resources::ObjectiveOption.where(:query_options => {:objective_item_ids => objective_ids})
+      # @objective_options = @objective_options.group_by { |option| option.objective_item_id}
+      @subjective_items = Vger::Resources::SubjectiveItem.active.all.to_a
+      @candidate_details = params[:report]
+      #create candidate_assessment
+      @candidate = Vger::Resources::Candidate.where(:query_options => { :email => params[:report][:candidate_email] }).all[0]
+      if @candidate.present?
+        candidate_assessment = @assessment.candidate_assessments.where(:query_options => {
+            :assessment_id => @assessment.id,
+            :candidate_id => @candidate.id
+          }).all[0]
+      else
+        candidate_data = {:name => params[:report][:candidate_name], :email => params[:report][:candidate_email]}
+        @candidate = candidate = Vger::Resources::Candidate.create(candidate_data)
+      end
+      options = {
+          :assessment_taker_type => params[:report][:candidate_type],
+          :report_link_receiver_name => params[:report][:candidate_name],
+          :report_link_receiver_email => current_user.email,
+          :assessment_link_receiver_name => params[:report][:candidate_name],
+          :assessment_link_receiver_email => current_user.email,
+          :send_report_links_to_manager => true,
+          :send_assessment_links_to_manager => true
+        }
+      # options.merge!(template_id: params[:template_id].to_i) if params[:template_id].present?
+      # create candidate_assessment if not present
+      # add it to list of candidate_assessments to send email
+      unless candidate_assessment
+        candidate_assessment = Vger::Resources::Suitability::CandidateAssessment.create(
+          :assessment_id => @assessment.id,
+          :candidate_id => @candidate.id,
+          :candidate_stage => params[:candidate_stage],
+          :responses_count => 0,
+          :report_email_recipients => current_user.email,
+          :options => options,
+          :language => @assessment.language
+        )
       end
     end
   end
