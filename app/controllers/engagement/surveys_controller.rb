ebe6be6df25fbaffec2b1b7b1e4acc9c4f05ec58
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2015-01-22 17:25:05 +0530

    Fix for 500 error on Create Engagement survey flow

--- app/controllers/engagement/surveys_controller.rb
+++ app/controllers/engagement/surveys_controller.rb
@@ -4,35 +4,35 @@ class Engagement::SurveysController < ApplicationController
   before_filter :get_company
   before_filter :get_survey, :except => [:index,:home]
   before_filter :get_defined_forms, :only => [:new, :edit, :create]
-  
+
   layout 'engagement'
 
   def api_resource
     Vger::Resources::Engagement::Survey
   end
-  
+
   def home
     order_by = params[:order_by] || "engagement_surveys.created_at"
     order_type = params[:order_type] || "DESC"
     order = "#{order_by} #{order_type}"
     @surveys = Vger::Resources::Engagement::Survey.where(query_options: {company_id: params[:company_id]}, order: order, page: params[:page], per: 10).all
-    @candidate_counts = Vger::Resources::Candidate.group_count(group: "engagement_candidate_surveys.survey_id", joins: :engagement_candidate_surveys, query_options: { 
-      "engagement_candidate_surveys.survey_id" => @surveys.map(&:id) 
+    @candidate_counts = Vger::Resources::Candidate.group_count(group: "engagement_candidate_surveys.survey_id", joins: :engagement_candidate_surveys, query_options: {
+      "engagement_candidate_surveys.survey_id" => @surveys.map(&:id)
     })
-    @completed_counts = Vger::Resources::Candidate.group_count(group: "engagement_candidate_surveys.survey_id", joins: :engagement_candidate_surveys, query_options: { 
+    @completed_counts = Vger::Resources::Candidate.group_count(group: "engagement_candidate_surveys.survey_id", joins: :engagement_candidate_surveys, query_options: {
       "engagement_candidate_surveys.survey_id" => @surveys.map(&:id),
       "engagement_candidate_surveys.status" => Vger::Resources::Engagement::CandidateSurvey.completed_statuses
     })
   end
-  
+
   def new
   end
-  
+
   def create
     params[:survey][:company_id] = @company.id
     @survey = Vger::Resources::Engagement::Survey.new(params[:survey])
     if @survey.save
-      if params[:defined_form_id].present?
+      if params[:defined_form_id].present? && params[:defined_form_id] =~ /^\d+$/
         factual_information_form = Vger::Resources::FormBuilder::FactualInformationForm.create({
           :defined_form_id => params[:defined_form_id],
           :company_id => @company.id,
@@ -47,7 +47,7 @@ class Engagement::SurveysController < ApplicationController
       render action: :new
     end
   end
-  
+
   def add_elements
     if request.get?
       get_elements
@@ -60,20 +60,20 @@ class Engagement::SurveysController < ApplicationController
         flash[:error] = @survey.error_messages.join("<br/>").html_safe
         render action: :new
       end
-    end  
+    end
   end
-  
+
   def details
     @total_candidates = Vger::Resources::Engagement::CandidateSurvey.count({
       survey_id: @survey.id
     })
   end
-  
+
   def elements
   end
-  
+
   protected
-  
+
   def get_company
     @company = Vger::Resources::Company.find(params[:company_id], :methods => [])
   end
@@ -85,7 +85,7 @@ class Engagement::SurveysController < ApplicationController
       @survey = Vger::Resources::Engagement::Survey.new
     end
   end
-  
+
   def get_elements
     @facets = Vger::Resources::Engagement::Facet.where({
       query_options: {
@@ -125,7 +125,7 @@ class Engagement::SurveysController < ApplicationController
     end
     @survey_elements = @survey_elements.group_by{|x| x.element.factor_id }
   end
-  
+
   def get_defined_forms
     @defined_forms = Vger::Resources::FormBuilder::DefinedForm.where(scopes: { for_company: @company.id }, query_options: { active: true }).all.to_a
   end
