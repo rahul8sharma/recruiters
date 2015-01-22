f490b84b08d4b7aa37d3b25be7650f89026a5da6
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2015-01-22 15:47:37 +0530

    Fixes the issue with FTs and MRFTs not getting added to competencies inspite of selecting them

--- app/controllers/suitability/competencies_controller.rb
+++ app/controllers/suitability/competencies_controller.rb
@@ -80,7 +80,7 @@ class Suitability::CompetenciesController < MasterDataController
     params[:mrf_trait_ids] ||= {}
     params[:functional_trait_ids] ||= {}
 
-    factor_ids = params[:factor_ids]\
+    factor_ids = params[:suitability_trait_ids]\
       .collect { |index,factor_hash| factor_hash.keys}\
       .flatten.map(&:to_i)
     mrf_trait_ids = params[:mrf_trait_ids]\
