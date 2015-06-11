aaa1e2bd7e40e7f7549c4974240262a77e1a696e
Prashant Chaudhari <prashant@jombay.com>
2015-06-11 15:03:16 +0530

    show section wise subjective and objective items

--- app/controllers/subjective_items_controller.rb
+++ app/controllers/subjective_items_controller.rb
@@ -8,9 +8,12 @@ class SubjectiveItemsController < MasterDataController
   def import_from
     "import_from_google_drive"
   end
+  
   def index_columns
-    [:id,:body, :active, :subjective_item_group_id]
+    [:id,:body, :behaviour, :active, :subjective_item_group_id, :section_id, :company_id]
   end
-
-
+  
+  def search_columns
+    [:id,:body, :behaviour, :active, :subjective_item_group_id, :section_id, :company_id]
+  end
 end
