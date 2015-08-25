a7c4b56bbb08bf002596b5c6da1d2711ed97d4ef
Prashant Chaudhari <prashant@jombay.com>
2015-08-25 19:39:24 +0530

    show dropdown for feedback if flag is enabled

--- app/controllers/suitability/candidate_assessment_report_feedbacks_controller.rb
+++ app/controllers/suitability/candidate_assessment_report_feedbacks_controller.rb
@@ -4,10 +4,11 @@ class Suitability::CandidateAssessmentReportFeedbacksController < ApplicationCon
   
   def create
     factor_feedback_scores_attributes = []
-    params[:feedback][:factor_feedback].each do |id,score|
+    params[:feedback][:factor_feedback].each do |id,data|
       factor_feedback_scores_attributes << {
         :candidate_factor_score_id => id,
-        :score => score
+        :score => data[:score],
+        :company_norm_bucket_id => data[:company_norm_bucket_id]
       }
     end
     @feedback = Vger::Resources::Suitability::CandidateAssessmentReportFeedback.new(:email => params[:feedback][:email],:candidate_assessment_id => params[:feedback][:candidate_assessment_id], :factor_feedback_scores_attributes => factor_feedback_scores_attributes)
