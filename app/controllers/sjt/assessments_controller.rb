0d8738514c1f3c488bb1f337f17ac1b574bfa4e6
abhishek verma <abhishek.verma@jombay.com>
2015-11-05 12:38:54 +0530

    Step-1: New Assessment

--- app/controllers/sjt/assessments_controller.rb
+++ app/controllers/sjt/assessments_controller.rb
@@ -1,6 +1,7 @@
 class Sjt::AssessmentsController < Suitability::CustomAssessmentsController
   layout 'sjt/sjt'
 
+
   def home
     order_by = params[:order_by] || "created_at"
     params[:order_by] ||= order_by
@@ -16,6 +17,28 @@ class Sjt::AssessmentsController < Suitability::CustomAssessmentsController
     )
     Rails.logger.ap("--LOGGER--")
     Rails.logger.ap(@assessments.size)
+  end
+
+  def create
+    respond_to do |format|
+      if @assessment.valid? and @assessment.save
+        if params[:defined_form_id].present?
+          factual_information_form = Vger::Resources::FormBuilder::FactualInformationForm.create({
+            :defined_form_id => params[:defined_form_id],
+            :company_id => @company.id,
+            :assessment_type => "Suitability::CustomAssessment",
+            :assessment_id => @assessment.id
+          })
+        end
+        redirect_path = competencies_company_sjt_assessment_path(:company_id => params[:company_id], :id => @assessment.id)
+        format.html { redirect_to redirect_path }
+      else
+        get_meta_data
+        add_set
+        flash[:error] = @assessment.error_messages.join("<br/>") rescue ""
+        format.html { render action: "new" }
+      end
+    end
   end
 
   def edit
