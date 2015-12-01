ce3f2f3ff28bac4618a5875e999953cc0ae3e127
abhishek verma <abhishek.verma@jombay.com>
2015-12-01 14:42:39 +0530

    OAC Routes and HTML FIles

---
+++ app/controllers/oac/assessments_controller.rb
@@ -0,0 +1,28 @@
+class Oac::AssessmentsController < ApplicationController
+  layout 'oac/oac'
+
+  def home  
+  end
+
+  def select_tools
+  end
+
+  def select_competencies
+  end
+
+  def set_weightage
+  end
+
+  def customize_assessments
+  end
+
+  def add_candidates
+  end
+
+  def send_assessment
+  end
+
+  def assign_assessor
+  end
+
+end
