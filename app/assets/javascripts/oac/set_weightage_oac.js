4204303ceeac2824168d3968a0739bd22efe0c42
abhishek verma <abhishek.verma@jombay.com>
2015-12-02 17:05:16 +0530

    Send Assessment

--- app/assets/javascripts/oac/set_weightage_oac.js
+++ app/assets/javascripts/oac/set_weightage_oac.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $('.tabs a').on('click',function(){
     $('.tabs a').removeClass('active');
     $('.boxes div').removeClass('show');
-    
     $(this).addClass('active');
     $('.'+ $(this).attr('class').split("_")[0]).addClass('show');
   })
