4761a2398a51d94c582fe8cdbb94d2cf088c28f0
Prashant Chuadhari <prashant@jombay.com>
2015-12-15 16:27:23 +0530

    fix feedback on reports issue

--- app/controllers/suitability/user_assessment_report_feedbacks_controller.rb
+++ app/controllers/suitability/user_assessment_report_feedbacks_controller.rb
@@ -6,13 +6,14 @@ class Suitability::UserAssessmentReportFeedbacksController < ApplicationControll
     factor_feedback_scores_attributes = []
     params[:feedback][:factor_feedback].each do |id,data|
       factor_feedback_scores_attributes << {
-        :user_factor_score_id => id,
+        :candidate_factor_score_id => id,
         :score => data[:score],
         :company_norm_bucket_id => data[:company_norm_bucket_id]
       }
     end
     @feedback = Vger::Resources::Suitability::UserAssessmentReportFeedback.new(:email => params[:feedback][:email],:user_assessment_id => params[:feedback][:user_assessment_id], :factor_feedback_scores_attributes => factor_feedback_scores_attributes)
     @feedback.save
+    Rails.logger.ap @feedback.error_messages
     render :action => :thank_you, :layout => "feedback_reports_thank_you_page"
   end
 end
