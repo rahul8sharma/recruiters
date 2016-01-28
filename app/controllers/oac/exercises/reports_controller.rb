24106f8b9a134806adc07b075e5449640961266e
abhishek verma <abhishek.verma@jombay.com>
2016-01-28 17:57:10 +0530

    Uploader enabled for PDF reports

--- app/controllers/oac/exercises/reports_controller.rb
+++ app/controllers/oac/exercises/reports_controller.rb
@@ -66,11 +66,11 @@ class Oac::Exercises::ReportsController < ApplicationController
   def s3_report
     report = Vger::Resources::Oac::UserExerciseReport.find(params[:report_id], params)
     url = S3Utils.get_url(report.html_bucket, report.html_key)
-    # if request.format.to_s == "application/pdf"
-    #   url = S3Utils.get_url(report.pdf_bucket, report.pdf_key)
-    # else
-    #   url = S3Utils.get_url(report.html_bucket, report.html_key)
-    # end
+    if request.format.to_s == "application/pdf"
+      url = S3Utils.get_url(report.pdf_bucket, report.pdf_key)
+    else
+      url = S3Utils.get_url(report.html_bucket, report.html_key)
+    end
     redirect_to url
   end
 
