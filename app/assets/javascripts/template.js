8ecb857805e84c1c210cc73e06e073fe7415c9e2
abhishek verma <abhishek.verma@jombay.com>
2016-03-09 11:31:36 +0530

    tweak

--- app/assets/javascripts/template.js
+++ app/assets/javascripts/template.js
@@ -64,11 +64,13 @@ jQuery(document).ready(function($){
   $(document).on("click",".template_variable_link",function(){
     var template_variable = "<$"+$(this).attr("template_variable_name")+"$>";
     if(currentElement.attr('id') == "template_html_editor"){
+      
       editor.trumbowyg('saveSelection');
-      var caretPosition = editor.trumbowyg('getSelection').startOffset;
+      var caretPosition = editor.trumbowyg('getSelection').startOffset == 0 ? 0 : editor.trumbowyg('getSelection').startOffset+3;
       var text = editor.trumbowyg('html');
       var insertText = [text.slice(0, caretPosition), template_variable, text.slice(caretPosition)].join('');
       editor.trumbowyg('html', insertText);
+      
       setTemplateBodyValue(editor.trumbowyg('html'));
     }else{
       currentElement.insertAtCaret(template_variable);
