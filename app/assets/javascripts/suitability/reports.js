8d73967bcf74868be9d06b9e17898bed4d68ff43
abhishek verma <abhishek.verma@jombay.com>
2016-03-28 11:48:48 +0530

    Code refined

--- app/assets/javascripts/suitability/reports.js
+++ app/assets/javascripts/suitability/reports.js
@@ -6,20 +6,4 @@ $(function() {
     numberOfMonths: 2,
     presetRanges: []
   });
-  $('#export_form').on('submit', function(event){
-  	$('#exports').removeClass('hide');
-  	$('.popup_modal').center();
-  	//  remove this return false when hooking it up with back-end
-  	return false;
-  });
-
-  $('#popup_close').on('click', function(){
-  	$('#exports').addClass('hide');
-  });
-
 });
-
-jQuery.fn.center = function () {
-	this.css("margin-top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
-	return this;
-}
