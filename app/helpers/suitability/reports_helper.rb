59bbd4ecd120d60a2ec5696b059979f3181024dd
abhishek verma <abhishek.verma@jombay.com>
2016-04-07 09:40:52 +0530

    export fields fixed

--- app/helpers/suitability/reports_helper.rb
+++ app/helpers/suitability/reports_helper.rb
@@ -3,21 +3,25 @@ module Suitability
     def get_export_fields_for_report_summary(assessment)
       fields = YAML.load(
         File.read(
-          "#{Rails.root}/config/exports/suitability/report_summary/jombay.yml"
+          "#{Rails.root}/config/exports/suitability/report_summary/fit.yml"
         )
       )
-      if assessment.brand_partner.present?
-        begin
-          file_name = assessment.brand_partner
-          fields.merge!(YAML.load(
-            File.read(
-              "#{Rails.root}/config/exports/suitability/report_summary/#{file_name}.yml"
-            )
-          ))
-        rescue Errno::ENOENT => e
-          Rails.logger.debug "No report summary configuration found for #{assessment.brand_partner}"
-        end    
-      end
+      begin
+        file_name = assessment.brand_partner
+        fields.merge!(YAML.load(
+          File.read(
+            "#{Rails.root}/config/exports/suitability/report_summary/#{file_name}.yml"
+          )
+        ))
+        fields.merge!(YAML.load(
+          File.read(
+            "#{Rails.root}/config/exports/suitability/report_summary/#{assessment.assessment_type}.yml"
+          )
+        ))
+
+      rescue Errno::ENOENT => e
+        Rails.logger.debug "No report summary configuration found for #{assessment.brand_partner} or for #{assessment.assessment_type}"
+      end    
       return fields
     end
   end
