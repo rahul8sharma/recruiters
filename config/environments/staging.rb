17b5aa5c6239d3dfb0864f9ff00a6e5ad120ff2f
Prashant Chuadhari <prashant@jombay.com>
2016-07-05 16:17:42 +0530

    disale ssl verification for staging and internal

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,5 @@
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 Recruiters::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
