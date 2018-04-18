403da30678aada6e8ab02f9521d7cf2c35f4889e
Chandrakant <jombay@jombay-chandu>
2018-04-18 17:37:16 +0530

    Create new assement popup

--- app/javascript/components/acdc/assessments/Index.vue
+++ app/javascript/components/acdc/assessments/Index.vue
@@ -1,16 +1,43 @@
 <template>
-  <div id="rahul">
+  <div class="wrapper">
     <Header></Header>
-    {{text}}
+    <div class="divider-2"></div>
+    <div class="container">
+      <a href="#" class="button btn-warning big big-text right uppercase">Create Assessment</a>
+      <div class="clr"></div>
+      <div class="modal scrollable">
+        <div class="modal-container large-20">
+          <div class="heading fs-14 uppercase">
+            <span>Create Assessment</span>
+            <div class="spacer"></div>
+            <div class="close">&times;</div>  
+          </div>
+          <div class="modal-body">
+            <CreateAssessment></CreateAssessment>
+          </div>
+          <div class="actions">
+            <div class="spacer"></div>
+            <a href="" class="button btn-link uppercase fs-14 black-7">Discard</a>
+            <a href="" disabled class="button btn-warning uppercase fs-14">Create Assessment</a>
+          </div>
+        </div>
+      </div>
+    </div>
+     
+        
+
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-    import Header from 'components/layout/Header.vue';
-    import Footer from 'components/layout/Footer.vue';
+    import Header from 'components/shared/Header.vue';
+    import CreateAssessment from 'components/acdc/assessments/New.vue';
+  //   import VueScrollbar from 'vue2-scrollbar';
+  // require("vue2-scrollbar/dist/style/vue2-scrollbar.css")
+    import Footer from 'components/shared/Footer.vue';
   export default {
-    components: { Header, Footer },
+    components: { Header, Footer, CreateAssessment },
     data () {
           return {
           text: 'Hello World!'
