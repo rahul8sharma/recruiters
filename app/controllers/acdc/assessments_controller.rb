c28adb1f2bf65d62bf8dbbeefca8690e1b453042
Akshay Sharma <akshay@jombay.com>
2018-04-18 18:49:12 +0530

    call API for acdc_assessment and stored in vuex

--- app/controllers/acdc/assessments_controller.rb
+++ app/controllers/acdc/assessments_controller.rb
@@ -3,19 +3,64 @@ class Acdc::AssessmentsController < ApplicationController
   before_action :authenticate_user!
   before_action { authorize_user!(params[:company_id]) }
   before_action :get_company
+  protect_from_forgery with: :null_session #TODO need to change
 
   layout 'acdc/acdc'
 
+  def api_resource
+    Vger::Resources::Acdc::Assessment
+  end
+
   def index
+    @assessment = api_resource.where(:query_options => { 
+                            :company_id => params[:company_id]
+                          }, 
+                          :page => params[:page], :per => 10,
+                          order: ["created_at DESC"]
+                        ).all
   end
 
   def edit
+    @assessment = api_resource.find(params[:id])
+    render json: @assessment.attributes, status: :ok
+  end
+
+  def show
+    @assessment = api_resource.find(params[:id])
+    render json: @assessment.attributes, status: :ok
   end
 
   def create
+    @assessment = api_resource.create(acdc_assessment_params)
+
+    if @assessment.error_messages && @assessment.error_messages.present?
+      render json: @assessment.errors, status: :unprocessable_entity
+    else
+      render json: @assessment.attributes, status: :created
+    end
   end
 
   def update
+    @assessment = api_resource.save_existing(params[:id], acdc_assessment_params)
+
+    if @assessment.error_messages && @assessment.error_messages.present?
+      render json: @assessment.errors, status: :unprocessable_entity  
+    else
+      render json: @assessment.attributes, status: :accepted
+    end
+  end
+
+  def get_meta_data
+    @functional_areas = Hash[Vger::Resources::FunctionalArea.where(:query_options => {:active=>true},:order => "name ASC")\
+                          .all.to_a.collect{|x| [x.id,x]}]
+    @industries = Hash[Vger::Resources::Industry.where(:query_options => {:active=>true},:order => "name ASC")\
+                        .all.to_a.collect{|x| [x.id,x]}]
+    @job_experiences = Hash[Vger::Resources::JobExperience.active.all.to_a.collect{|x| [x.id,x]}]
+    render json: { 
+                   'functional_areas': @functional_areas, 
+                   'industries': @industries,
+                   'job_experiences': @job_experiences 
+                 }, status: :ok
   end
 
   private
---
+++ app/javascript/components/acdc/assessments/assessment_crud.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<hr>
+      <span>
+        <button @click='create_assessment'>Create Default Assessment</button>
+        <button @click='update_assessment'>Update Default Assessment</button>
+        <button @click='show_assessment'>Show Default Assessment</button>
+        <button @click='get_meta_data'>fetch fucntion area, exprinces, Industry</button>
+      </span>
+    <hr>
+	</div>	
+</template>
+
+<script type="text/javascript">
+	export default {
+		methods: {
+			create_assessment: function () {
+			 this.$store.dispatch('update_acdc_assessment')
+			},
+			update_assessment: function () {
+			 this.$store.dispatch('update_acdc_assessment')
+			},
+			show_assessment: function () {
+			 this.$store.dispatch('update_acdc_assessment')
+			},
+			get_meta_data: function () {
+			 this.$store.dispatch('fetch_meta_data')
+			}
+    }
+	}
+</script>
