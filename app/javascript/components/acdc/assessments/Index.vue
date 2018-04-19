150383c1ff6ff96684d083ce872790e0f3ec5031
Rahul Sharma <rahul.sharma@jombay.com>
2018-04-19 13:15:23 +0530

    Added "set_acdc_assessment" method to get ACDC assessment. Added method to create ACDC assessment.

--- app/javascript/components/acdc/assessments/Index.vue
+++ app/javascript/components/acdc/assessments/Index.vue
@@ -6,7 +6,14 @@
     <div class="container">
       <button @click="setModalState" class="button btn-warning big big-text right uppercase">Create Assessment</button>
       <div class="clr"></div>
-      <CreateAssessment v-bind:isModalOpen="modal.isOpen" v-bind:isCreateSubmitButtonEnable="isCreateSubmitButtonEnable" v-bind:assessment="assessment" :set-modal-state="setModalState"></CreateAssessment>
+      <CreateAssessment
+        v-bind:isModalOpen="modal.isOpen"
+        v-bind:isCreateSubmitButtonEnable="isCreateSubmitButtonEnable"
+        v-bind:assessment="assessment"
+        :set-modal-state="setModalState"
+        :create-assessment="createAssessment"
+      >
+      </CreateAssessment>
     </div>
     <Footer></Footer>
   </div>
@@ -34,6 +41,17 @@
     methods: {
       setModalState () {
         this.modal.isOpen = !this.modal.isOpen;
+      },
+      createAssessment () {
+          console.log("Called...")
+          this.$store.dispatch('create_acdc_assessment', {
+              acdc_assessment: {
+                  name: this.assessment.name,
+                  company_id: 2,
+                  user_id: 176698,
+                  raw_data: {tool: this.assessment.tool}
+              }
+          })
       }
     },
     computed: {
