9daa5435e3ba55b0f115c0f9a601b5cdf1cac459
Chandrakant <jombay@jombay-chandu>
2018-04-23 21:38:50 +0530

    Loader UI fix

--- app/javascript/components/shared/loader.vue
+++ app/javascript/components/shared/loader.vue
@@ -1,10 +1,31 @@
-  <template>
+<template>
   <div class="loaderContainer">
-    <i class="fs-40">loading..........</i>
+    <div class="loader_icon">
+      Loading.................
+    </div>
+
+
   </div>        
 </template>
 
 <style lang="sass" scoped>
+  .loader_icon
+    width: 30%
+    overflow: hidden
+    font-size: 80px
+    animation: arrow-spin 2s 0s cubic-bezier(0.165, 0.840, 0.440, 1.000) infinite
+    color: #fff
+
+    @keyframes arrow-spin
+      0%
+        width: 0px
+      50%
+        width: 15%
+      100%
+        width: 30%
+        /*transform: rotate(360deg)*/
+
+      
   .loaderContainer
     position: fixed
     top: 0
@@ -16,7 +37,14 @@
     z-index: 999999
     justify-content: center
     align-items: center
-    display: none
+    //display: block
+    display: -webkit-box
+    display: -moz-box
+    display: -ms-flexbox
+    display: -webkit-flex
+    display: flex
+    align-items: center
+    justify-content: center
     &.loaderOpen
       display: -webkit-box
       display: -moz-box
