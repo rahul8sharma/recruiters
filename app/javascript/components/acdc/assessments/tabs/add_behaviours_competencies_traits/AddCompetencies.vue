3b9f4b9960dc41d6cf0810e6f941a45f0dde6c49
Chandrakant <jombay@jombay-chandu>
2018-04-27 18:11:53 +0530

    Add traits - Html/CSS

---
+++ app/javascript/components/acdc/assessments/tabs/add_behaviours_competencies_traits/AddCompetencies.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="edit_section">
+    <em class="fs-12 black-6 large-12 show">(Type Competencies name to add them in this Assessment, You can arrange the order of the competencies in next step)</em>
+    <div class="divider-1"></div>
+    <div class="large-1 columns"></div>
+    <div class="fs-12 large-13 columns color-warning">
+      Type Competency name
+    </div>
+    <div class="fs-12 large-3 columns color-warning">
+      Weightage
+    </div>
+    <div class="clr"></div>
+
+    <ul>
+      <li>
+        <div class="fs-16 large-1 columns black-9 line-height-4">1.</div>
+
+        <div class="form-group large-7 columns">
+          <input type="text" placeholder="Competency name" />
+          <label>Competency name</label>
+        </div>
+
+        <em class="large-6 columns fs-12 black-6 line-height-3">
+          11/20 characters
+        </em>
+        <div class="form-group large-3 columns">
+          <input type="text" placeholder="Weightage" />
+          <label>Weightage</label>
+        </div>
+
+        <div class="large-2 columns"></div>
+
+        <a href="" class="shuffle_action fs-12 uppercase black-6 text-center line-height-3 bold large-3 columns">Move Down</a>
+        <a href="" class="shuffle_action fs-12 uppercase black-6 text-center line-height-3 bold large-3 columns">Move up</a>
+        <div class="clr"></div>
+        
+      </li>
+    </ul>
+    <div class="divider-2"></div>
+    
+    <button class="button btn-warning btn-link  uppercase fs-16 bold">+ Add More Competency</button>
+    
+    <div class="divider-2"></div>
+    
+    <a href="#" class="more_actions_btn uppercase fs-12 bold open">
+      More Actions
+    </a>
+    <em class="fs-12 black-6">(Competency Ranges, Competency Scores, Consistency Report)</em>
+
+    <div class="more_actions_container open">
+      <div class="divider-1"></div>
+      <div class="clearfix">
+        <div class="large-10 columns">
+          <label class="custom-checkbox">
+            <input type="checkbox"/>
+            <div class="label-text fs-12">Show competency score on report</div>
+          </label>
+        </div>
+        <div class="large-10 columns">
+          <label class="custom-checkbox">
+            <input type="checkbox"/>
+            <div class="label-text fs-12">Show consistency score on report</div>
+          </label>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+<style lang="sass" scoped>
+  .shuffle_action
+    &:hover
+      opacity: 0.6
+  .more_actions_btn
+    padding-right: 15px
+    background: url('~assets/images/ic-dropright-warning.svg') no-repeat right center
+    margin-right: 16px
+    &.open
+      background: url('~assets/images/ic-dropdown-warning.svg') no-repeat right center 
+  .more_actions_container
+    overflow: hidden
+    display: none
+    padding-top: 15px
+    &.open
+      display: block
+</style>
+<script>
+  export default {
+  }
+</script>
---
+++ app/javascript/components/acdc/assessments/tabs/add_behaviours_competencies_traits/AddTraits.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="edit_section">
+
+    <div class="section_title uppercase fs-12 bold">
+      1.  Lorem Ipsum Competency
+    </div>
+    <div class="divider-2"></div>
+    <div class="fs-12 large-8 columns color-warning pr-20">Type Trait name</div>
+    <div class="fs-12 large-7 columns color-warning pr-20">Range from</div>
+    <div class="fs-12 large-7 columns color-warning pr-20">Range to</div>
+    <div class="fs-12 large-5 columns color-warning pr-20">Weightage</div>
+    <div class="clr"></div>
+
+    <ul>
+      <li>
+
+        <div class="form-group large-8 columns pr-20">
+          <input type="text" placeholder="Type Trait name" />
+          <label>Trait name</label>
+        </div>
+        <div class="select-box large-7 columns pr-20">
+          <div class="form-group">
+            <multi-select
+              placeholder="Range from">
+            </multi-select>
+            <label>Range from</label>
+          </div>
+        </div>
+        <div class="select-box large-7 columns pr-20">
+          <div class="form-group">
+            <multi-select
+              placeholder="Range to">
+            </multi-select>
+            <label>Range to</label>
+          </div>
+        </div>
+
+        
+        <div class="form-group large-5 columns pr-20">
+          <input type="text" placeholder="Weightage" />
+          <label>Weightage</label>
+        </div>
+
+        <div class="clr"></div>
+        
+      </li>
+    </ul>
+    <div class="divider-2"></div>
+    
+    <button class="button btn-warning btn-link  uppercase fs-16 bold">+ Add More Traits</button>
+    
+    <div class="divider-2"></div>
+    
+    <a href="#" class="more_actions_btn uppercase fs-12 bold open">
+      More Actions
+    </a>
+    <em class="fs-12 black-6">(Competency Range)</em>
+
+    <div class="more_actions_container open">
+      <div class="divider-1"></div>
+      <div class="clearfix">
+        <div class="large-10 columns">
+          <label class="custom-checkbox">
+            <input type="checkbox"/>
+            <div class="label-text fs-12">Show Consistency Scores on report</div>
+          </label>
+        </div>
+       
+      </div>
+
+    </div>
+
+  </div>
+</template>
+<style lang="sass" scoped>
+  $color-warning: #ff8308
+  .section_title
+    color: $color-warning
+    padding: 8px
+    background: #fff4e9
+  .more_actions_btn
+    padding-right: 15px
+    background: url('~assets/images/ic-dropright-warning.svg') no-repeat right center
+    margin-right: 16px
+    &.open
+      background: url('~assets/images/ic-dropdown-warning.svg') no-repeat right center 
+  .more_actions_container
+    overflow: hidden
+    display: none
+    padding-top: 15px
+    &.open
+      display: block
+</style>
+<script>
+  export default {
+  }
+</script>
