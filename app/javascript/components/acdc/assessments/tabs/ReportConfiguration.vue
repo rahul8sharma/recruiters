c0aaf30c44fb2dbc9dc4fe8f5c24a44b2ed8046b
Rahul Sharma <rahul.sharma@jombay.com>
2018-05-08 23:14:58 +0530

    Added report preview.

--- app/javascript/components/acdc/assessments/tabs/ReportConfiguration.vue
+++ app/javascript/components/acdc/assessments/tabs/ReportConfiguration.vue
@@ -1,11 +1,13 @@
 <template>
-	<div class="edit_section">
-		<button @click="changeReportType()">Html Report</button>
-		<button @click="changeReportType()">PDF Report</button>
-		<v-jstree v-if="isPdfReport" :data="htmlData" show-checkbox multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
-		<v-jstree v-if="!isPdfReport" :data="pdfData" show-checkbox multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
-	<div class="divider-4"></div>
-	</div>
+  <div class="edit_section">
+    <button @click="changeReportType()">Html Report</button>
+    <button @click="changeReportType()">PDF Report</button>
+    <v-jstree v-if="isHtmlReport" :data="htmlData" show-checkbox multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
+    <v-jstree v-if="!isHtmlReport" :data="pdfData" show-checkbox multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
+    <button @click="generateReportPreview()">Generate {{isHtmlReport ? "HTML" : "PDF"}} Preview</button>
+    <iframe class="hide" id="reportConfigIframe" style="height: 1000px;width:99.7%;margin:auto;overflow:auto;"></iframe>
+    <div class="divider-4"></div>
+  </div>
 </template>
 
 <script>
@@ -14,7 +16,7 @@
     components: { VJstree },
     data () {
       return  {
-        isPdfReport: true
+        isHtmlReport: true
       }
     },
     methods: {
@@ -23,7 +25,24 @@
         console.log(node.model.text + ' clicked !')
       },
       changeReportType() {
-        this.isPdfReport = !this.isPdfReport
+        this.isHtmlReport = !this.isHtmlReport
+        document.getElementById("reportConfigIframe").classList.add("hide");
+      },
+      generateReportPreview() {
+        let cloneConfig = {}
+        let selectedReportConfig = []
+        if (this.isHtmlReport) {
+          cloneConfig = JSON.parse(JSON.stringify(this.htmlData));
+          selectedReportConfig = { html: { sections: getSelectedReportConfig(cloneConfig) } }
+        } else {
+          cloneConfig = JSON.parse(JSON.stringify(this.pdfData));
+          selectedReportConfig = { pdf: { sections: getSelectedReportConfig(cloneConfig) } }
+        }
+        let viewMode = this.isHtmlReport ? "html" : "pdf"
+        this.$store.dispatch('getreportPreview', {
+          config: selectedReportConfig,
+          viewMode: viewMode
+        })
       }
     },
     created: function () {
@@ -40,6 +59,32 @@
       error () {
         return this.$store.getters.errorReportConfiguration
       }
+    },
+    watch: {
+      '$store.state.ReportConfigurationStore.reportPreview' (newCount, oldCount) {
+        let reportPreviewConfig = this.$store.getters.reportPreview
+        if (reportPreviewConfig.length != 0) {
+          document.getElementById('reportConfigIframe').classList.remove("hide");
+          document.getElementById('reportConfigIframe').contentWindow.document.body.innerHTML = reportPreviewConfig
+        }
+      }
     }
+  }
+
+  function getSelectedReportConfig(reportConfig) {
+    for(var k=0; k<reportConfig.length; k++) {
+      if (reportConfig[k].selected) {
+        reportConfig[k]["state"] = { "selected": true}
+        delete reportConfig[k].opened
+        delete reportConfig[k].selected
+        delete reportConfig[k].value
+        if (typeof reportConfig[k] == "object" && reportConfig[k].children != null) {getSelectedReportConfig(reportConfig[k].children);
+        }
+      } else {
+        reportConfig.splice(k, 1);
+        k = k - 1;
+      }
+    };
+    return reportConfig;
   }
 </script>
