31470a819de3c73909ae97ee360446880f5b13fb
Rahul Sharma <rahul.sharma@jombay.com>
2018-05-09 13:12:13 +0530

    Saved report configurations.

---
+++ app/javascript/config/deepMerge.js
@@ -0,0 +1,56 @@
+// Get the selected report json from report config.
+
+function getSelectedReportConfig(reportConfig) {
+  for(var k=0; k<reportConfig.length; k++) {
+    if (reportConfig[k].selected) {
+      reportConfig[k]["state"] = { "selected": true}
+      delete reportConfig[k].opened
+      delete reportConfig[k].selected
+      delete reportConfig[k].value
+      if (typeof reportConfig[k] == "object" && reportConfig[k].children != null) {getSelectedReportConfig(reportConfig[k].children);
+      }
+    } else {
+      reportConfig.splice(k, 1);
+      k = k - 1;
+    }
+  };
+  return reportConfig;
+}
+
+// Deep merge of two array json.
+
+var found = false;
+
+function deepMerge(target, selected) {
+  target.forEach( function (targetedItem) {
+    found = false;
+    var selectedItem = getObjectBykey(selected, targetedItem.id)
+    if (selectedItem) {
+      targetedItem['opened'] = true
+      targetedItem['selected'] = true
+    } else {
+      targetedItem['opened'] = false
+      targetedItem['selected'] = false
+    }
+    targetedItem['value'] = targetedItem.text
+    if (typeof targetedItem == "object" && targetedItem.children != null) {
+      deepMerge(targetedItem.children, selected);
+    }
+  });
+  return target;
+}
+
+// Get json object by key.
+
+function getObjectBykey(jsonObjects, id) {
+  jsonObjects.forEach( function (jsonObject) {
+    if(jsonObject.id === id) {
+      found = true;
+    } else if (typeof jsonObject == "object" && jsonObject.children != null && !found) {
+      getObjectBykey(jsonObject.children, id);
+    }
+  });
+  return found;
+}
+
+export default { deepMerge, getObjectBykey, getSelectedReportConfig  }
--- app/javascript/store/report_configurations/index.js
+++ app/javascript/store/report_configurations/index.js
@@ -2,23 +2,31 @@ import Vue from 'vue/dist/vue.esm';
 
 export default {
   state: {
-    htmlReportConfiguration: [],
-    pdfReportConfiguration: [],
+    htmlSelectedConfig: [],
+    pdfSelectedConfig: [],
     errorReportConfiguration: [],
-    reportPreview: ""
+    reportPreview: "",
+    htmlConfig: {},
+    pdfConfig: {}
   },
   mutations: {
-    setHtmlReportConfiguration (state, payload) {
-      state.htmlReportConfiguration = payload
+    sethtmlSelectedConfig (state, payload) {
+      state.htmlSelectedConfig = payload
     },
-    setPdfReportConfiguration (state, payload) {
-      state.pdfReportConfiguration = payload
+    setpdfSelectedConfig (state, payload) {
+      state.pdfSelectedConfig = payload
     },
     setErrorReportConfiguration (state, payload) {
       state.errorReportConfiguration = payload
     },
     setReportPreview (state, payload) {
       state.reportPreview = payload
+    },
+    setHtmlConfig (state, payload) {
+      state.htmlConfig = payload
+    },
+    setPdfConfig (state, payload) {
+      state.pdfConfig = payload
     }
   },
   actions: {
@@ -28,9 +36,11 @@ export default {
         .then(function (response) {
           let selected_config = JSON.parse(response.data.selected);
           let config = JSON.parse(response.data.config);
+          commit('setHtmlConfig', config.html.sections);
+          commit('setPdfConfig', config.pdf.sections);
+          commit('sethtmlSelectedConfig', selected_config.html.sections);
+          commit('setpdfSelectedConfig', selected_config.pdf.sections);
           commit('setErrorReportConfiguration', response.data.error);
-          commit('setHtmlReportConfiguration', deepMerge(config.html.sections,selected_config.html.sections));
-          commit('setPdfReportConfiguration', deepMerge(config.pdf.sections,selected_config.pdf.sections));
         })
         .catch(error => {
           console.log(error)
@@ -60,52 +70,23 @@ export default {
     }
   },
   getters: {
-    htmlReportConfiguration (state) {
-      return state.htmlReportConfiguration
+    htmlSelectedConfig (state) {
+      return state.htmlSelectedConfig
     },
-    pdfReportConfiguration (state) {
-      return state.pdfReportConfiguration
+    pdfSelectedConfig (state) {
+      return state.pdfSelectedConfig
     },
     errorReportConfiguration (state) {
       return state.errorReportConfiguration
     },
     reportPreview (state) {
       return state.reportPreview
+    },
+    htmlConfig (state) {
+      return state.htmlConfig
+    },
+    pdfConfig (state) {
+      return state.pdfConfig
     }
   }
 };
-
-
-// Javascript code for deep merge of two array json.
-
-var found = false;
-
-function deepMerge(target, selected) {
-  target.forEach( function (targetedItem) {
-    found = false;
-    var selectedItem = getObjectBykey(selected, targetedItem.id)
-    if (selectedItem) {
-      targetedItem['opened'] = true
-      targetedItem['selected'] = true
-    } else {
-      targetedItem['opened'] = false
-      targetedItem['selected'] = false
-    }
-    targetedItem['value'] = targetedItem.text
-    if (typeof targetedItem == "object" && targetedItem.children != null) {
-      deepMerge(targetedItem.children, selected);
-    }
-  });
-  return target;
-}
-
-function getObjectBykey(jsonObjects, id) {
-  jsonObjects.forEach( function (jsonObject) {
-    if(jsonObject.id === id) {
-      found = true;
-    } else if (typeof jsonObject == "object" && jsonObject.children != null && !found) {
-      getObjectBykey(jsonObject.children, id);
-    }
-  });
-  return found;
-}
